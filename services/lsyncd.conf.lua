c2d640724ff1bcf73058417e5c9a164097bd2abd
aelassas <akram.elassas@gmail.com>
2022-05-12 07:00:24 +0100

    Add services

---
+++ services/lsyncd.conf.lua
@@ -0,0 +1,31 @@
+settings {
+  logfile = "/var/log/lsyncd/lsyncd.log",
+  statusFile = "/var/log/lsyncd/lsyncd.status",
+  statusInterval = 20,
+  nodaemon   = false
+}
+
+sync {
+  default.rsync,
+  source = "/opt/wexstream",
+  target = "/mnt/sdb/wexstream"
+}
+
+sync {
+  default.rsync,
+  source = "/opt/bookcars",
+  target = "/mnt/sdb/bookcars"
+}
+
+sync {
+ default.rsync,
+ source = "/home/aelassas/bookcars",
+ target = "/mnt/sdb/__bookcars__"
+}
+
+
+sync {
+ default.rsync,
+ source = "/home/aelassas/wexstream",
+ target = "/mnt/sdb/__wexstream__"
+}
