d302d9648a2dd5fb173356d8ac0ef5ee963e78f4
aelassas <akram.elassas@gmail.com>
2022-05-23 19:06:54 +0100

    Update SeatsList.js

--- backend/src/elements/SeatsList.js
+++ backend/src/elements/SeatsList.js
@@ -52,6 +52,7 @@ class SeatsList extends Component {
                     <MenuItem value={4}>4</MenuItem>
                     <MenuItem value={5}>5</MenuItem>
                     <MenuItem value={6}>6</MenuItem>
+                    <MenuItem value={7}>7</MenuItem>
                     <MenuItem value={8}>8</MenuItem>
                     <MenuItem value={9}>9</MenuItem>
                 </Select>
