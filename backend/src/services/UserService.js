d4ebed15107edf2ffc5cd232c96ab16769e1726f
aelassas <akram.elassas@gmail.com>
2022-05-23 19:47:16 +0100

    Update package.json

--- backend/src/services/UserService.js
+++ backend/src/services/UserService.js
@@ -69,14 +69,13 @@ export default class UserService {
 
         const _signout = () => {
             const deleteAllCookies = () => {
-                const cookies = document.cookie.split(";");
+                const cookies = document.cookie.split(';');
 
                 for (let i = 0; i < cookies.length; i++) {
                     const cookie = cookies[i];
-                    const eqPos = cookie.indexOf("=");
-                    // const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+                    const eqPos = cookie.indexOf('=');
                     const name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
-                    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+                    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
                 }
             };
 
