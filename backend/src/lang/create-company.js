da34fcbb7b32db65191bf6d958c6a3cf9cbdd85d
aelassas <akram.elassas@gmail.com>
2022-05-23 20:36:12 +0100

    Fixed some issues

--- backend/src/lang/create-company.js
+++ backend/src/lang/create-company.js
@@ -5,13 +5,13 @@ import UserService from '../services/UserService';
 export const strings = new LocalizedStrings({
     fr: {
         CREATE_COMPANY_HEADING: 'Nouvelle société',
-        INVALID_COMPANY_NAME: 'Cette société existe déjà',
+        INVALID_COMPANY_NAME: 'Cette société existe déjà.',
         COMPANY_IMAGE_SIZE_ERROR: `L'image doit être au format ${Env.COMPANY_IMAGE_WIDTH}x${Env.COMPANY_IMAGE_HEIGHT}`,
         RECOMMENDED_IMAGE_SIZE: `Taille d'image recommandée : ${Env.COMPANY_IMAGE_WIDTH}x${Env.COMPANY_IMAGE_HEIGHT}`
     },
     en: {
         CREATE_COMPANY_HEADING: 'New company',
-        INVALID_COMPANY_NAME: 'This company already exists',
+        INVALID_COMPANY_NAME: 'This company already exists.',
         COMPANY_IMAGE_SIZE_ERROR: `The image must be in the format ${Env.COMPANY_IMAGE_WIDTH}x${Env.COMPANY_IMAGE_HEIGHT}`,
         RECOMMENDED_IMAGE_SIZE: `Recommended image size: ${Env.COMPANY_IMAGE_WIDTH}x${Env.COMPANY_IMAGE_HEIGHT}`
     }
