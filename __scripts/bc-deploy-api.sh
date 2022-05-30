176e31506a05f22a4e14be43d842e3613fe0344d
aelassas <akram.elassas@gmail.com>
2022-05-31 00:31:41 +0100

    Update scripts

--- __scripts/bc-deploy-api.sh
+++ __scripts/bc-deploy-api.sh
@@ -3,13 +3,11 @@
 echo "Deploying BookCars API..."
 
 cd /opt/bookcars
-#git reset --hard
 git pull
 chmod +x -R /opt/bookcars/__scripts
 
 cd /opt/bookcars/api
-#npm install
-#npm update
+
 npm ci
 npm prune
 sudo systemctl restart bookcars
