ace4605cb8bad9b4e1818454c6f859238e0988f6
aelassas <akram.elassas@gmail.com>
2022-05-31 00:31:41 +0100

    Update scripts

--- __scripts/bc-deploy-backend.sh
+++ __scripts/bc-deploy-backend.sh
@@ -3,20 +3,15 @@
 echo "Deploying BookCars backend..."
 
 cd /opt/bookcars
-#git reset --hard
 git pull
 sudo chmod +x -R /opt/bookcars/__scripts
 
 cd /opt/bookcars/backend
 sudo rm -rf build
-#npm install
-#npm update
+
 npm ci
 npm prune
 npm run build
-
-#sudo systemctl restart bookcars-backend
-#sudo systemctl status bookcars-backend --no-pager
 
 sudo rm -rf /var/www/bookcars.ma/backend
 sudo mkdir -p /var/www/bookcars.ma/backend
--- __scripts/bc-deploy-frontend.sh
+++ __scripts/bc-deploy-frontend.sh
@@ -3,20 +3,15 @@
 echo "Deploying BookCars frontend..."
 
 cd /opt/bookcars
-#git reset --hard
 git pull
 sudo chmod +x -R /opt/bookcars/__scripts
 
 cd /opt/bookcars/frontend
 sudo rm -rf build
-#npm install
-#npm update
+
 npm ci
 npm prune
 npm run build
-
-#sudo systemctl restart bookcars-frontend
-#sudo systemctl status bookcars-frontend --no-pager
 
 sudo rm -rf /var/www/bookcars.ma/frontend
 sudo mkdir -p /var/www/bookcars.ma/frontend
