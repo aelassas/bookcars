1a3887f9345eaca33b8c7b05ed995278f186bb38
aelassas <akram.elassas@gmail.com>
2022-06-01 22:17:52 +0100

    Update scripts

--- __scripts/bc-deploy-backend.sh
+++ __scripts/bc-deploy-backend.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+start_time=`date +%s`
 echo "Deploying BookCars backend..."
 
 cd /opt/bookcars
@@ -16,6 +17,10 @@ sudo rm -rf /var/www/bookcars.ma/backend
 sudo mkdir -p /var/www/bookcars.ma/backend
 sudo cp -rf build/* /var/www/bookcars.ma/backend
 
-echo "BookCars backend deployed."
+finish_time=`date +%s`
+elapsed_time=$((finish_time  - start_time))
+((sec=elapsed_time%60, elapsed_time/=60, min=elapsed_time%60, hrs=elapsed_time/60))
+timestamp=$(printf "BookCars backend deployed in %d minutes and %d seconds." $min $sec)
+echo $timestamp
 
 #$SHELL
--- __scripts/bc-deploy-frontend.sh
+++ __scripts/bc-deploy-frontend.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+start_time=`date +%s`
 echo "Deploying BookCars frontend..."
 
 cd /opt/bookcars
@@ -16,6 +17,10 @@ sudo rm -rf /var/www/bookcars.ma/frontend
 sudo mkdir -p /var/www/bookcars.ma/frontend
 sudo cp -rf build/* /var/www/bookcars.ma/frontend
 
-echo "BookCars frontend deployed."
+finish_time=`date +%s`
+elapsed_time=$((finish_time  - start_time))
+((sec=elapsed_time%60, elapsed_time/=60, min=elapsed_time%60, hrs=elapsed_time/60))
+timestamp=$(printf "BookCars frontend deployed in %d minutes and %d seconds." $min $sec)
+echo $timestamp
 
 #$SHELL
