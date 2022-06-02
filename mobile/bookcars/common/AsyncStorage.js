8dbcda8dbab44ff3f98df64e6353f8606655f5cc
aelassas <akram.elassas@gmail.com>
2022-06-02 23:59:17 +0100

    Update mobile app

--- mobile/bookcars/common/AsyncStorage.js
+++ mobile/bookcars/common/AsyncStorage.js
@@ -1,21 +1,30 @@
 import ReactAsyncStorage from '@react-native-async-storage/async-storage';
+import Toast from 'react-native-root-toast';
+import i18n from '../lang/i18n';
+
+const error = (err) => {
+    console.log(err);
+    Toast.show(i18n.t('GENERIC_ERROR'), {
+        duration: Toast.durations.LONG,
+    });
+}
 
 export default class AsyncStorage {
 
     static async storeString(key, value) {
         try {
             await ReactAsyncStorage.setItem(key, value);
-        } catch (e) {
-            // saving error
+        } catch (err) {
+            error(err);
         }
     };
 
     static async getString(key) {
         try {
-            const value = await ReactAsyncStorage.getItem(key)
+            const value = await ReactAsyncStorage.getItem(key);
             return value;
-        } catch (e) {
-            // error reading value
+        } catch (err) {
+            error('test');
         }
     };
 
@@ -23,26 +32,26 @@ export default class AsyncStorage {
         try {
             const jsonValue = JSON.stringify(value);
             await ReactAsyncStorage.setItem(key, jsonValue);
-        } catch (e) {
-            // saving error
+        } catch (err) {
+            error('test');
         }
     };
 
     static async getObject(key) {
         try {
-            const value = await ReactAsyncStorage.getItem(key)
+            const value = await ReactAsyncStorage.getItem(key);
             const jsonValue = value != null ? JSON.parse(value) : null;
             return jsonValue;
-        } catch (e) {
-            // error reading value
+        } catch (err) {
+            error('test');
         }
     };
 
     static async removeItem(key) {
         try {
             await ReactAsyncStorage.removeItem(key);
-        } catch (e) {
-            // remove error
+        } catch (err) {
+            error('test');
         }
     };
 }
--- mobile/bookcars/elements/LocationSelectList.js
+++ mobile/bookcars/elements/LocationSelectList.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { Text } from 'react-native';
 import Env from '../config/env.config';
 import { AutocompleteDropdown } from 'react-native-autocomplete-dropdown';
+import Toast from 'react-native-root-toast';
 import i18n from '../lang/i18n';
-import Helper from '../common/Helper';
 import LocationService from '../services/LocationService';
 
 import Feather from 'react-native-vector-icons/Feather'
@@ -17,11 +17,10 @@ class LocationSelectList extends Component {
             init: false,
             loading: false,
             rows: [],
+            rowCount: 0,
             fetch: false,
             page: 1,
             keyword: '',
-            selectedOptions: [],
-            rowCount: 0,
             selectedItem: null
         };
     }
@@ -41,22 +40,27 @@ class LocationSelectList extends Component {
     fetch = () => {
         const { rows, keyword, page } = this.state;
         this.setState({ loading: true });
-        
+
         LocationService.getLocations(keyword, page, Env.PAGE_SIZE)
             .then(data => {
+
                 const _data = data.length > 0 ? data[0] : {};
                 if (_data.length === 0) _data.resultData = [];
                 const _rows = page === 1 ? _data.resultData.map(location => ({
                     id: location._id,
                     title: location.name
                 })) : [...rows, ..._data.resultData];
-                this.setState({ rows: _rows, loading: false, fetch: _data.resultData.length > 0 });
+                this.setState({ rows: _rows, loading: false, fetch: _data.resultData.length > 0 }, () => {
+                    if (this.props.onFetch) {
+                        this.props.onFetch();
+                    }
+                });
             })
             .catch((err) => {
                 console.log(err);
-                // TODO
-                // toast(commonStrings.GENERIC_ERROR, { type: 'error' })
-                // TODO AsyncStorage toast + console.log
+                Toast.show(i18n.t('GENERIC_ERROR'), {
+                    duration: Toast.durations.LONG,
+                });
             });
     };
 
@@ -70,10 +74,6 @@ class LocationSelectList extends Component {
         return null;
     }
 
-    componentDidMount() {
-        this.fetch();
-    }
-
     render() {
         const { loading, rows, selectedItem } = this.state;
 
@@ -81,41 +81,46 @@ class LocationSelectList extends Component {
             <AutocompleteDropdown
                 containerStyle={this.props.style}
                 initialValue={(selectedItem && selectedItem.id) || ''}
-                onSelectItem={item => {
-                    item && this.setSelectedItem(item.id)
-                }}
                 loading={loading}
                 useFilter={false} // set false to prevent rerender twice
                 dataSet={rows}
+                onSelectItem={item => {
+                    item && this.setSelectedItem(item.id)
+                }}
                 onChangeText={this.onChangeText}
+                onClear={() => {
+                    this.setSelectedItem(null);
+                }
+                }
                 textInputProps={{
                     placeholder: this.props.label || '',
+                    placeholderTextColor: 'rgba(0, 0, 0, 0.6)',
                     autoCorrect: false,
                     autoCapitalize: 'none',
                     style: {
                         borderRadius: 25,
                         paddingLeft: 18,
-                        height: 60,
-                    },
+                    }
                 }}
                 rightButtonsContainerStyle={{
                     right: 8,
-                    height: 60,
+                    height: 30,
                     alignSelf: 'center',
                 }}
                 inputContainerStyle={{
-                    borderRadius: 25,
-                    height: 60
+                    borderRadius: 25
                 }}
                 suggestionsListContainerStyle={{
-                    marginTop: 15
+                    display: this.props.close ? 'none' : undefined
                 }}
                 renderItem={(item, text) => <Text style={{ padding: 15 }}>{item.title}</Text>}
-                inputHeight={50}
-                showChevron={true}
-                closeOnBlur={false}
-                clearOnFocus={false}
-                closeOnSubmit={false}
+                inputHeight={60}
+                showChevron={false}
+                showClear={selectedItem !== null}
+                closeOnBlur={true}
+                clearOnFocus={true}
+                closeOnSubmit={true}
+                EmptyResultComponent={<></>}
             />
         );
     }
