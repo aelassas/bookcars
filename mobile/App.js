1e726edbfd97733b3fd565b103598daf8a79a892
aelassas <akram.elassas@gmail.com>
2022-06-15 13:19:11 +0100

    Update mobile app

--- mobile/App.js
+++ mobile/App.js
@@ -17,6 +17,7 @@ export default function App() {
       try {
         // Keep the splash screen visible while we fetch resources
         await SplashScreen.preventAutoHideAsync();
+        await new Promise(resolve => setTimeout(resolve, 1500));
       } catch (err) {
         Helper.error(err);
       } finally {
