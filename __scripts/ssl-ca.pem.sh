66537b747cef02ecb3c83fa0a0249a7ba20815dd
aelassas <akram.elassas@gmail.com>
2022-06-20 23:12:06 +0100

    Update scripts

--- __scripts/ssl-ca.pem.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo Generating bookcars.ma.ca.pem...
-sudo rm -f bookcars.ma.ca.pem
-cat AAACertificateServices.crt SectigoRSADomainValidationSecureServerCA.crt USERTrustRSAAAACA.crt > bookcars.ma.ca.pem
-sudo chmod 644 bookcars.ma.pem
-echo bookcars.ma.ca.pem generated.
