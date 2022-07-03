7839d74d4350589c0199487d68441872172f0e66
aelassas <akram.elassas@gmail.com>
2022-07-03 16:30:09 +0100

    Update scripts

--- __scripts/bc-deploy-frontend.sh
+++ __scripts/bc-deploy-frontend.sh
@@ -16,6 +16,10 @@ sudo rm -rf /var/www/bookcars.ma/frontend
 sudo mkdir -p /var/www/bookcars.ma/frontend
 sudo cp -rf build/* /var/www/bookcars.ma/frontend
 
+sudo rm -rf /var/cache/nginx
+sudo systemctl restart nginx
+sudo systemctl status nginx --no-pager
+
 finish_time=`date +%s`
 elapsed_time=$((finish_time  - start_time))
 ((sec=elapsed_time%60, elapsed_time/=60, min=elapsed_time%60, hrs=elapsed_time/60))
