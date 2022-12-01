553c6c60e45b3abfb79608788e92b8b014a356be
aelassas <akram.elassas@gmail.com>
2022-12-01 12:03:47 +0100

    Update api helper

--- api/common/Helper.js
+++ api/common/Helper.js
@@ -1,16 +1,13 @@
-export default class Helper {
-
-    static joinURL(part1, part2) {
-        if (part1.charAt(part1.length - 1) === '/') {
-            part1 = part1.substr(0, part1.length - 1);
-        }
-        if (part2.charAt(0) === '/') {
-            part2 = part2.substr(1);
-        }
-        return part1 + '/' + part2;
+export const joinURL = (part1, part2) => {
+    if (part1.charAt(part1.length - 1) === '/') {
+        part1 = part1.substr(0, part1.length - 1);
     }
-
-    static clone(obj) {
-        return JSON.parse(JSON.stringify(obj));
+    if (part2.charAt(0) === '/') {
+        part2 = part2.substr(1);
     }
+    return part1 + '/' + part2;
+};
+
+export const clone = (obj) => {
+    return JSON.parse(JSON.stringify(obj));
 };
