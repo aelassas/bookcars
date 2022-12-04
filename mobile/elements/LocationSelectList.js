22358aa830f63625f364fca649cb9f1668f790f2
aelassas <akram.elassas@gmail.com>
2022-12-04 14:53:08 +0100

    Update LocationSelectList

--- mobile/elements/LocationSelectList.js
+++ mobile/elements/LocationSelectList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import Env from '../config/env.config';
 import { AutocompleteDropdown } from './AutocompleteDropdown/AutocompleteDropdown'
@@ -6,148 +6,127 @@ import { MaterialIcons } from '@expo/vector-icons';
 import * as LocationService from '../services/LocationService';
 import * as Helper from '../common/Helper';
 
-class LocationSelectList extends Component {
+const LocationSelectList = (props) => {
+    const [loading, setLoading] = useState(false);
+    const [rows, setRows] = useState([]);
+    const [selectedItem, setSelectedItem] = useState([]);
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            init: false,
-            loading: false,
-            rows: [],
-            rowCount: 0,
-            fetch: false,
-            page: 1,
-            text: '',
-            selectedItem: null
-        };
-    }
+    useEffect(() => {
+        setSelectedItem(props.selectedItem);
+    }, [props.selectedItem]);
 
-    setSelectedItem = (selectedItem) => {
-        this.setState({ selectedItem });
+    const _setSelectedItem = (selectedItem) => {
+        setSelectedItem(selectedItem);
 
-        if (this.props.onSelectItem) {
-            this.props.onSelectItem(selectedItem);
+        if (props.onSelectItem) {
+            props.onSelectItem(selectedItem);
         }
     };
 
-    onChangeText = (text) => {
-        this.setState({ text }, () => this.fetch());
+    const onChangeText = (text) => {
+        _fetch(text);
     };
 
-    fetch = () => {
-        const { rows, text, page } = this.state;
-        this.setState({ loading: true });
+    const _fetch = (text) => {
+        setLoading(true);
 
-        LocationService.getLocations(text, page, Env.PAGE_SIZE)
+        LocationService.getLocations(text, 1, Env.PAGE_SIZE)
             .then(data => {
                 const _data = data.length > 0 ? data[0] : {};
                 if (_data.length === 0) _data.resultData = [];
-                const _rows = page === 1 ? _data.resultData.map(location => ({
+                const _rows = _data.resultData.map(location => ({
                     id: location._id,
                     title: location.name
-                })) : [...rows, ..._data.resultData];
-                this.setState({ rows: _rows, loading: false, fetch: _data.resultData.length > 0 }, () => {
-                    if (this.props.onFetch) {
-                        this.props.onFetch();
-                    }
-                });
+                }));
+                setRows(_rows);
+                if (props.onFetch) {
+                    props.onFetch();
+                }
+                setLoading(false);
             })
             .catch((err) => {
                 Helper.error(err);
             });
     };
 
-    static getDerivedStateFromProps(nextProps, prevState) {
-        const { selectedItem } = prevState;
+    const small = props.size === 'small';
 
-        if (nextProps.selectedItem != selectedItem) {
-            return { selectedItem: nextProps.selectedItem };
-        }
-
-        return null;
-    }
-
-    render() {
-        const { loading, rows, selectedItem } = this.state;
-        const small = this.props.size === 'small';
-
-        return (
-            <View style={{ ...this.props.style, ...styles.container }}>
-                <Text style={{
-                    display: selectedItem !== null ? 'flex' : 'none',
-                    backgroundColor: this.props.backgroundColor ?? '#fafafa',
-                    color: 'rgba(0, 0, 0, 0.6)',
-                    fontSize: 12,
-                    fontWeight: '400',
-                    paddingRight: 5,
-                    paddingLeft: 5,
-                    marginLeft: 15,
-                    position: 'absolute',
-                    top: -8,
-                    zIndex: 1,
-                }}>{this.props.label}</Text>
-                <AutocompleteDropdown
-                    blur={this.props.blur}
-                    initialValue={(selectedItem && selectedItem.id) || ''}
-                    loading={loading}
-                    useFilter={false} // set false to prevent rerender twice
-                    dataSet={rows}
-                    onSelectItem={item => {
-                        item && this.setSelectedItem(item.id)
-                    }}
-                    onChangeText={(text) => {
-                        this.onChangeText(text);
-                        if (this.props.onChangeText) this.props.onChangeText(text);
-                    }}
-                    onClear={() => {
-                        this.setSelectedItem(null);
-                    }}
-                    onFocus={() => {
-                        if (this.props.onFocus) this.props.onFocus();
-                    }}
-                    textInputProps={{
-                        placeholder: this.props.label || '',
-                        placeholderTextColor: 'rgba(0, 0, 0, 0.6)',
-                        autoCorrect: false,
-                        autoCapitalize: 'none',
-                        style: {
-                            borderRadius: 10,
-                            paddingLeft: 15,
-                            fontSize: small ? 14 : 16,
-                        }
-                    }}
-                    rightButtonsContainerStyle={{
-                        right: 8,
-                        height: 30,
-                        alignSelf: 'center',
-                    }}
-                    inputContainerStyle={{
-                        backgroundColor: this.props.backgroundColor ?? '#fafafa',
-                        color: 'rgba(0, 0, 0, 0.87)',
-                        borderColor: 'rgba(0, 0, 0, 0.23)',
-                        borderWidth: 1,
+    return (
+        <View style={{ ...props.style, ...styles.container }}>
+            <Text style={{
+                display: selectedItem !== null ? 'flex' : 'none',
+                backgroundColor: props.backgroundColor ?? '#fafafa',
+                color: 'rgba(0, 0, 0, 0.6)',
+                fontSize: 12,
+                fontWeight: '400',
+                paddingRight: 5,
+                paddingLeft: 5,
+                marginLeft: 15,
+                position: 'absolute',
+                top: -8,
+                zIndex: 1,
+            }}>{props.label}</Text>
+            <AutocompleteDropdown
+                blur={props.blur}
+                initialValue={(selectedItem && selectedItem.id) || ''}
+                loading={loading}
+                useFilter={false} // set false to prevent rerender twice
+                dataSet={rows}
+                onSelectItem={item => {
+                    item && _setSelectedItem(item.id)
+                }}
+                onChangeText={(text) => {
+                    onChangeText(text);
+                    if (props.onChangeText) props.onChangeText(text);
+                }}
+                onClear={() => {
+                    _setSelectedItem(null);
+                }}
+                onFocus={() => {
+                    if (props.onFocus) props.onFocus();
+                }}
+                textInputProps={{
+                    placeholder: props.label || '',
+                    placeholderTextColor: 'rgba(0, 0, 0, 0.6)',
+                    autoCorrect: false,
+                    autoCapitalize: 'none',
+                    style: {
                         borderRadius: 10,
-                    }}
-                    suggestionsListContainerStyle={{
-                        display: this.props.close ? 'none' : 'flex'
-                    }}
-                    renderItem={(item, text) => (
-                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                            <MaterialIcons name="location-on" size={23} style={{ marginLeft: 5 }} />
-                            <Text style={{ paddingTop: 15, paddingRight: 5, paddingBottom: 15, paddingLeft: 5 }}>{item.title}</Text>
-                        </View>
-                    )}
-                    inputHeight={small ? 37 : 55}
-                    showChevron={false}
-                    showClear={selectedItem !== null}
-                    closeOnBlur={true}
-                    clearOnFocus={false}
-                    closeOnSubmit={true}
-                    EmptyResultComponent={<></>}
-                />
-            </View>
-        );
-    }
+                        paddingLeft: 15,
+                        fontSize: small ? 14 : 16,
+                    }
+                }}
+                rightButtonsContainerStyle={{
+                    right: 8,
+                    height: 30,
+                    alignSelf: 'center',
+                }}
+                inputContainerStyle={{
+                    backgroundColor: props.backgroundColor ?? '#fafafa',
+                    color: 'rgba(0, 0, 0, 0.87)',
+                    borderColor: 'rgba(0, 0, 0, 0.23)',
+                    borderWidth: 1,
+                    borderRadius: 10,
+                }}
+                suggestionsListContainerStyle={{
+                    display: props.close ? 'none' : 'flex'
+                }}
+                renderItem={(item, text) => (
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <MaterialIcons name="location-on" size={23} style={{ marginLeft: 5 }} />
+                        <Text style={{ paddingTop: 15, paddingRight: 5, paddingBottom: 15, paddingLeft: 5 }}>{item.title}</Text>
+                    </View>
+                )}
+                inputHeight={small ? 37 : 55}
+                showChevron={false}
+                showClear={selectedItem !== null}
+                closeOnBlur={true}
+                clearOnFocus={false}
+                closeOnSubmit={true}
+                EmptyResultComponent={<></>}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
