fca17ee0ea31ba1aefa4ae458a1fe7d86fb1d13a
aelassas <akram.elassas@gmail.com>
2022-12-08 15:26:48 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-const SignIn = lazy(() => import("./components/SignIn"));
-const SignUp = lazy(() => import("./components/SignUp"));
+const SignIn = lazy(() => import('./components/SignIn'));
+const SignUp = lazy(() => import('./components/SignUp'));
 const Activate = lazy(() => import('./components/Activate'));
 const ForgotPassword = lazy(() => import('./components/ForgotPassword'));
 const ResetPassword = lazy(() => import('./components/ResetPassword'));
-const Home = lazy(() => import("./components/Home"));
-const Cars = lazy(() => import("./components/Cars"));
-const CreateBooking = lazy(() => import("./components/CreateBooking"));
-const Bookings = lazy(() => import("./components/Bookings"));
-const Booking = lazy(() => import("./components/Booking"));
-const Settings = lazy(() => import("./components/Settings"));
-const Notifications = lazy(() => import("./components/Notifications"));
-const ToS = lazy(() => import("./components/ToS"));
-const About = lazy(() => import("./components/About"));
-const ChangePassword = lazy(() => import("./components/ChangePassword"));
-const Contact = lazy(() => import("./components/Contact"));
-const NoMatch = lazy(() => import("./components/NoMatch"));
+const Home = lazy(() => import('./components/Home'));
+const Cars = lazy(() => import('./components/Cars'));
+const CreateBooking = lazy(() => import('./components/CreateBooking'));
+const Bookings = lazy(() => import('./components/Bookings'));
+const Booking = lazy(() => import('./components/Booking'));
+const Settings = lazy(() => import('./components/Settings'));
+const Notifications = lazy(() => import('./components/Notifications'));
+const ToS = lazy(() => import('./components/ToS'));
+const About = lazy(() => import('./components/About'));
+const ChangePassword = lazy(() => import('./components/ChangePassword'));
+const Contact = lazy(() => import('./components/Contact'));
+const NoMatch = lazy(() => import('./components/NoMatch'));
 
 const App = () => (
 	<Router>
