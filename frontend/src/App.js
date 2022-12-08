3fa55220c50ba17625c417527b700cc3a865c1f8
aelassas <akram.elassas@gmail.com>
2022-12-08 15:18:14 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,35 +19,33 @@ const ChangePassword = lazy(() => import("./components/ChangePassword"));
 const Contact = lazy(() => import("./components/Contact"));
 const NoMatch = lazy(() => import("./components/NoMatch"));
 
-const App = () => {
-	return (
-		<Router>
-			<div className="App">
-				<Suspense fallback={<></>}>
-					<Routes>
-						<Route exact path="/sign-in" element={<SignIn />} />
-						<Route exact path="/sign-up" element={<SignUp />} />
-						<Route exact path='/activate' element={<Activate />} />
-						<Route exact path='/forgot-password' element={<ForgotPassword />} />
-						<Route exact path='/reset-password' element={<ResetPassword />} />
-						<Route exact path="/" element={<Home />} />
-						<Route exact path="/cars" element={<Cars />} />
-						<Route exact path="/create-booking" element={<CreateBooking />} />
-						<Route exact path="/bookings" element={<Bookings />} />
-						<Route exact path="/booking" element={<Booking />} />
-						<Route exact path="/settings" element={<Settings />} />
-						<Route exact path="/notifications" element={<Notifications />} />
-						<Route exact path="/change-password" element={<ChangePassword />} />
-						<Route exact path="/about" element={<About />} />
-						<Route exact path="/tos" element={<ToS />} />
-						<Route exact path="/contact" element={<Contact />} />
+const App = () => (
+	<Router>
+		<div className="App">
+			<Suspense fallback={<></>}>
+				<Routes>
+					<Route exact path="/sign-in" element={<SignIn />} />
+					<Route exact path="/sign-up" element={<SignUp />} />
+					<Route exact path='/activate' element={<Activate />} />
+					<Route exact path='/forgot-password' element={<ForgotPassword />} />
+					<Route exact path='/reset-password' element={<ResetPassword />} />
+					<Route exact path="/" element={<Home />} />
+					<Route exact path="/cars" element={<Cars />} />
+					<Route exact path="/create-booking" element={<CreateBooking />} />
+					<Route exact path="/bookings" element={<Bookings />} />
+					<Route exact path="/booking" element={<Booking />} />
+					<Route exact path="/settings" element={<Settings />} />
+					<Route exact path="/notifications" element={<Notifications />} />
+					<Route exact path="/change-password" element={<ChangePassword />} />
+					<Route exact path="/about" element={<About />} />
+					<Route exact path="/tos" element={<ToS />} />
+					<Route exact path="/contact" element={<Contact />} />
 
-						<Route path="*" element={<NoMatch />} />
-					</Routes>
-				</Suspense>
-			</div>
-		</Router>
-	);
-};
+					<Route path="*" element={<NoMatch />} />
+				</Routes>
+			</Suspense>
+		</div>
+	</Router>
+);
 
 export default App;
