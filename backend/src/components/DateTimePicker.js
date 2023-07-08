07a16180f881608b8303a46a3c769d066838c73c
aelassas <akram.elassas@gmail.com>
2023-07-08 12:40:26 +0100

    Bumped npm packages to latest versions

--- backend/src/components/DateTimePicker.js
+++ backend/src/components/DateTimePicker.js
@@ -1,9 +1,7 @@
 import React, { useState } from 'react'
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
-import { MobileDateTimePicker  as MuiDateTimePicker } from '@mui/x-date-pickers/MobileDateTimePicker'
-// import { TextField, IconButton, InputAdornment } from '@mui/material'
-// import { Clear as ClearIcon } from '@mui/icons-material'
+import { DateTimePicker  as MuiDateTimePicker } from '@mui/x-date-pickers/DateTimePicker'
 import { fr, enUS } from "date-fns/locale"
 
 const DateTimePicker = (props) => {
@@ -27,39 +25,11 @@ const DateTimePicker = (props) => {
                 slotProps={{
                     textField: {
                         variant: props.variant || 'standard',
+                    },
+                    actionBar: {
+                        actions: ['accept', 'cancel', 'today', 'clear']
                     }
                 }}
-                // renderInput={(params) =>
-                //     <TextField
-                //         {...params}
-                //         variant={props.variant || 'standard'}
-                //         fullWidth
-                //         required={props.required}
-                //         autoComplete='off'
-                //         InputProps={{
-                //             ...params.InputProps,
-                //             endAdornment:
-                //                 <>
-                //                     {
-                //                         value && (
-                //                             <InputAdornment position='end' className='d-adornment'>
-                //                                 <IconButton
-                //                                     size='small'
-                //                                     onClick={() => {
-                //                                         setValue(null)
-                //                                         if (props.onChange) props.onChange(null)
-                //                                     }}>
-                //                                     <ClearIcon className='d-adornment-icon' />
-                //                                 </IconButton>
-                //                             </InputAdornment>
-                //                         )
-                //                     }
-                //                     {params.InputProps.endAdornment}
-                //                 </>
-                //         }}
-                //     />
-                // }
-
             />
         </LocalizationProvider>
     )
