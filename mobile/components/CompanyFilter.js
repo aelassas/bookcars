b3d7643c38353d4175cf6f3f7de5068997bcade8
aelassas <akram.elassas@gmail.com>
2023-07-26 19:32:58 +0100

    Upgrade to Expo SDK 49.0.5

--- mobile/components/CompanyFilter.js
+++ mobile/components/CompanyFilter.js
@@ -4,23 +4,23 @@ import { Image, Pressable, StyleSheet, View } from 'react-native'
 import * as Helper from '../common/Helper'
 import Env from '../config/env.config'
 import i18n from '../lang/i18n'
-import * as CompanyService from '../services/CompanyService'
+import * as SupplierService from '../services/SupplierService'
 import Link from './Link'
 import Switch from './Switch'
 import Accordion from './Accordion'
 
 const CompanyFilter = (props) => {
-    const [companies, setCompanies] = useState([])
-    const [checkedCompanies, setCheckedCompanies] = useState([])
+    const [suppliers, setSuppliers] = useState([])
+    const [checkedSuppliers, setCheckedSuppliers] = useState([])
     const [allChecked, setAllChecked] = useState(true)
 
     const init = async () => {
         try {
-            const allCompanies = await CompanyService.getAllCompanies()
-            const companies = allCompanies.map((company) => ({ ...company, checked: true }))
-            const checkedCompanies = Helper.flattenCompanies(companies)
-            setCompanies(companies)
-            setCheckedCompanies(checkedCompanies)
+            const allSuppliers = await SupplierService.getAllSuppliers()
+            const suppliers = allSuppliers.map((supplier) => ({ ...supplier, checked: true }))
+            const checkedCompanies = Helper.flattenCompanies(suppliers)
+            setSuppliers(suppliers)
+            setCheckedSuppliers(checkedCompanies)
             if (props.onLoad) props.onLoad(checkedCompanies)
         } catch (err) {
             Helper.error(err)
@@ -32,42 +32,42 @@ const CompanyFilter = (props) => {
     }, [])
 
     return (
-        companies.length > 0 && props.visible &&
+        suppliers.length > 0 && props.visible &&
         <View style={{ ...styles.container, ...props.style }}>
             <Accordion style={styles.accordion} title={i18n.t('SUPPLIER')}>
                 <View style={styles.companies}>
-                    {companies.map((company, index) => (
-                        <View key={company._id} style={styles.company}>
+                    {suppliers.map((supplier, index) => (
+                        <View key={supplier._id} style={styles.company}>
                             <Switch
-                                value={company.checked}
+                                value={supplier.checked}
                                 onValueChange={(checked) => {
                                     if (checked) {
-                                        company.checked = true
-                                        setCompanies(Helper.clone(companies))
-                                        checkedCompanies.push(company._id)
+                                        supplier.checked = true
+                                        setSuppliers(Helper.clone(suppliers))
+                                        checkedSuppliers.push(supplier._id)
 
-                                        if (checkedCompanies.length === companies.length) {
+                                        if (checkedSuppliers.length === suppliers.length) {
                                             setAllChecked(true)
                                         }
                                     } else {
-                                        company.checked = false
-                                        setCompanies(Helper.clone(companies))
-                                        const index = checkedCompanies.indexOf(company._id)
-                                        checkedCompanies.splice(index, 1)
+                                        supplier.checked = false
+                                        setSuppliers(Helper.clone(suppliers))
+                                        const index = checkedSuppliers.indexOf(supplier._id)
+                                        checkedSuppliers.splice(index, 1)
 
-                                        if (checkedCompanies.length === 0) {
+                                        if (checkedSuppliers.length === 0) {
                                             setAllChecked(false)
                                         }
                                     }
 
                                     if (props.onChange) {
-                                        props.onChange(Helper.clone(checkedCompanies))
+                                        props.onChange(Helper.clone(checkedSuppliers))
                                     }
                                 }}
                             >
                                 <Image
                                     style={styles.image}
-                                    source={{ uri: Helper.joinURL(Env.CDN_USERS, company.avatar) }}
+                                    source={{ uri: Helper.joinURL(Env.CDN_USERS, supplier.avatar) }}
                                 />
                             </Switch>
                         </View>
@@ -75,25 +75,25 @@ const CompanyFilter = (props) => {
                     )}
                     <Link style={styles.link} textStyle={styles.linkText} label={allChecked ? i18n.t('UNCHECK_ALL') : i18n.t('CHECK_ALL')} onPress={() => {
 
-                        let _checkedCompanies = []
+                        let _checkedSuppliers = []
                         if (allChecked) {
-                            companies.forEach((company) => {
-                                company.checked = false
+                            suppliers.forEach((supplier) => {
+                                supplier.checked = false
                             })
                             setAllChecked(false)
-                            setCompanies(Helper.clone(companies))
-                            setCheckedCompanies(_checkedCompanies)
+                            setSuppliers(Helper.clone(suppliers))
+                            setCheckedSuppliers(_checkedSuppliers)
                         } else {
-                            companies.forEach((company) => {
-                                company.checked = true
+                            suppliers.forEach((supplier) => {
+                                supplier.checked = true
                             })
                             setAllChecked(true)
-                            setCompanies(Helper.clone(companies))
-                            _checkedCompanies = Helper.clone(Helper.flattenCompanies(companies))
-                            setCheckedCompanies(_checkedCompanies)
+                            setSuppliers(Helper.clone(suppliers))
+                            _checkedSuppliers = Helper.clone(Helper.flattenCompanies(suppliers))
+                            setCheckedSuppliers(_checkedSuppliers)
 
                             if (props.onChange) {
-                                props.onChange(_checkedCompanies)
+                                props.onChange(_checkedSuppliers)
                             }
                         }
                     }} />
---
+++ mobile/services/SupplierService.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+import Env from '../config/env.config'
+import * as UserService from './UserService'
+import * as AxiosHelper from '../common/AxiosHelper'
+
+AxiosHelper.init(axios)
+
+export const getAllSuppliers = () => (
+    axios.get(`${Env.API_HOST}/api/all-suppliers`).then(res => res.data)
+)
+
+export const getSuppliers = (keyword, page, size) => (
+    axios.get(`${Env.API_HOST}/api/suppliers/${page}/${size}/?s=${encodeURIComponent(keyword)}`, { headers: UserService.authHeader() }).then(res => res.data)
+)
