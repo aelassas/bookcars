1370dffedb71e5f747df0cd4b439aa944a35ee94
aelassas <akram.elassas@gmail.com>
2023-07-26 20:30:48 +0100

    General fixes

--- __scripts/bc-deploy-frontend.sh
+++ __scripts/bc-deploy-frontend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-start_time=`date +%s`
+start_time=$(date +%s)
 echo "Deploying BookCars frontend..."
 
 cd /opt/bookcars
@@ -20,7 +20,7 @@ sudo rm -rf /var/cache/nginx
 sudo systemctl restart nginx
 sudo systemctl status nginx --no-pager
 
-finish_time=`date +%s`
+finish_time=$(date +%s)
 elapsed_time=$((finish_time  - start_time))
 ((sec=elapsed_time%60, elapsed_time/=60, min=elapsed_time%60, hrs=elapsed_time/60))
 timestamp=$(printf "BookCars frontend deployed in %d minutes and %d seconds." $min $sec)
