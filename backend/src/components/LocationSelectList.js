82b5d232f1766174491c394025927ff9120ebb2d
aelassas <akram.elassas@gmail.com>
2023-07-28 13:08:35 +0100

    Fix fetch

--- backend/src/components/LocationSelectList.js
+++ backend/src/components/LocationSelectList.js
@@ -28,7 +28,7 @@ const LocationSelectList = (props) => {
                 .then(data => {
                     const _data = data.length > 0 ? data[0] : {}
                     if (_data.length === 0) _data.resultData = []
-                    const totalRecords = _data.pageInfo.length > 0 ? _data.pageInfo[0].totalRecords : 0
+                    const totalRecords =  _data && _data.pageInfo && Array.isArray(_data.pageInfo) && _data.pageInfo.length > 0 ? _data.pageInfo[0].totalRecords : 0
                     const _rows = page === 1 ? _data.resultData : [...rows, ..._data.resultData]
 
                     setRows(_rows)
--- backend/src/components/UserList.js
+++ backend/src/components/UserList.js
@@ -62,7 +62,7 @@ const UserList = (props) => {
             .then(data => {
                 const _data = data.length > 0 ? data[0] : {}
                 if (_data.length === 0) _data.resultData = []
-                const totalRecords = _data.pageInfo.length > 0 ? _data.pageInfo[0].totalRecords : 0
+                const totalRecords =  _data && _data.pageInfo && Array.isArray(_data.pageInfo) && _data.pageInfo.length > 0 ? _data.pageInfo[0].totalRecords : 0
                 const _rows = _data.resultData
                 setRows(_rows)
                 setRowCount(totalRecords)
