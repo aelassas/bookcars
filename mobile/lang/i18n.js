5260ddb62c7b0ae16be1697bd0c914d3d8350667
aelassas <akram.elassas@gmail.com>
2023-08-06 20:03:19 +0100

    Update i18n.js

--- mobile/lang/i18n.js
+++ mobile/lang/i18n.js
@@ -1,6 +1,6 @@
 import { I18n } from 'i18n-js'
-import en from './en'
-import fr from './fr'
+import { en } from './en'
+import { fr } from './fr'
 
 const i18n = new I18n({ en, fr })
 i18n.enableFallback = true
