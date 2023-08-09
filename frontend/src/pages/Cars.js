8ef757a83e5ed3e4f759d158a1eb71229a83e2a5
aelassas <akram.elassas@gmail.com>
2023-08-09 08:42:00 +0100

    General cleanup

--- frontend/src/pages/Cars.js
+++ frontend/src/pages/Cars.js
@@ -86,11 +86,11 @@ const Cars = () => {
     }
     if (params.has('f')) {
       const val = params.get('f')
-      from = Helper.isInteger(val) && new Date(parseInt(val))
+      from = Helper.isInteger(val) && new Date(Number.parseInt(val))
     }
     if (params.has('t')) {
       const val = params.get('t')
-      to = Helper.isInteger(val) && new Date(parseInt(val))
+      to = Helper.isInteger(val) && new Date(Number.parseInt(val))
     }
 
     if (!pickupLocationId || !dropOffLocationId || !from || !to) {
