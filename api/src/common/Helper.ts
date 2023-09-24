5d879bd5eb81e1a91ad54e2797740221665ef56c
aelassas <akram.elassas@gmail.com>
2023-09-24 11:38:38 +0100

    Add jsdoc to Helper.ts and MailHelper.ts

--- api/src/common/Helper.ts
+++ api/src/common/Helper.ts
@@ -1,5 +1,13 @@
 import fs from 'node:fs/promises'
+import path from 'node:path'
 
+/**
+ * Convert string to boolean.
+ *
+ * @export
+ * @param {string} input
+ * @returns {boolean}
+ */
 export function StringToBoolean(input: string): boolean {
     try {
         return Boolean(JSON.parse(input.toLowerCase()))
@@ -8,6 +16,14 @@ export function StringToBoolean(input: string): boolean {
     }
 }
 
+/**
+ * Check if a file exists.
+ *
+ * @export
+ * @async
+ * @param {string} path
+ * @returns {Promise<boolean>}
+ */
 export async function exists(path: string): Promise<boolean> {
     try {
         await fs.access(path)
@@ -17,6 +33,14 @@ export async function exists(path: string): Promise<boolean> {
     }
 }
 
+/**
+ * Join two url parts.
+ *
+ * @export
+ * @param {string} part1
+ * @param {string} part2
+ * @returns {string}
+ */
 export function joinURL(part1: string, part2: string): string {
     if (part1.charAt(part1.length - 1) === '/') {
         part1 = part1.substring(0, part1.length - 1)
@@ -27,4 +51,15 @@ export function joinURL(part1: string, part2: string): string {
     }
 
     return `${part1}/${part2}`
+}
+
+/**
+ * Get filename without extension.
+ *
+ * @export
+ * @param {string} filename
+ * @returns {string}
+ */
+export function getFilenameWithoutExtension(filename: string): string {
+    return path.parse(filename).name
 }
