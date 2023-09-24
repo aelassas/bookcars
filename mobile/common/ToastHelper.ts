1301c86796e01fd6b7901be49b20d01d83eb2ec9
aelassas <akram.elassas@gmail.com>
2023-09-24 20:15:44 +0100

    Add jsdoc to mobile app

--- mobile/common/ToastHelper.ts
+++ mobile/common/ToastHelper.ts
@@ -1,12 +1,23 @@
 import Toast from 'react-native-root-toast'
 import i18n from '../lang/i18n'
 
+/**
+ * Toast a message.
+ *
+ * @param {string} message
+ */
 export const toast = (message: string) => {
     Toast.show(message, {
       duration: Toast.durations.LONG,
     })
   }
   
+/**
+ * Toast an error message.
+ *
+ * @param {?unknown} [err]
+ * @param {boolean} [__toast__=true]
+ */
 export const error = (err?: unknown, __toast__ = true) => {
     if (err) {
       console.log(err)
--- mobile/config/env.config.ts
+++ mobile/config/env.config.ts
@@ -1,5 +1,4 @@
 import {
-  BC_APP_TYPE,
   BC_API_HOST,
   BC_DEFAULT_LANGUAGE,
   BC_PAGE_SIZE,
@@ -14,27 +13,149 @@ import {
   BC_MINIMUM_AGE,
 } from '@env'
 
-const EN = 'en' // English ISO 639-1 language code
-const FR = 'fr' // French ISO 639-1 language code
+/**
+ * English ISO 639-1 language code.
+ *
+ * @type {'en'}
+ */
+const EN = 'en'
 
-export const APP_TYPE: string = BC_APP_TYPE || 'frontend'
+/**
+ * French ISO 639-1 language code.
+ *
+ * @type {'fr'}
+ */
+const FR = 'fr'
+
+/**
+ * Application type.
+ *
+ * @type {string}
+ */
+export const APP_TYPE: string = 'frontend'
+
+/**
+ * API host.
+ *
+ * @type {string}
+ */
 export const API_HOST: string = BC_API_HOST
+
+/**
+ * Number of maximum axios retries.
+ *
+ * @type {number}
+ */
 export const AXIOS_RETRIES: number = 3
+
+/**
+ * Axios retries interval in milliseconds.
+ *
+ * @type {number}
+ */
 export const AXIOS_RETRIES_INTERVAL: number = 500  // in milliseconds
+
+/**
+ * Languages.
+ *
+ * @type {string[]}
+ */
 export const LANGUAGES: string[] = [EN, FR]
+
+/**
+ * Default language. Default is English.
+ *
+ * @type {string}
+ */
 export const DEFAULT_LANGUAGE: string = BC_DEFAULT_LANGUAGE || EN
+
+/**
+ * Languages.
+ *
+ * @type {{ EN: 'en', FR: 'fr' }}
+ */
 export const LANGUAGE: { EN: 'en', FR: 'fr' } = { EN, FR }
-export const AXIOS_TIMEOUT: number = 5000 // in milliseconds
+
+/**
+ * Axios timeout in milliseconds.
+ *
+ * @type {number}
+ */
+export const AXIOS_TIMEOUT: number = 5000
+
+/**
+ * Page size. Default is 20.
+ *
+ * @type {number}
+ */
 export const PAGE_SIZE: number = Number.parseInt(BC_PAGE_SIZE) || 20
+
+/**
+ * Cars page size. Default is 8.
+ *
+ * @type {number}
+ */
 export const CARS_PAGE_SIZE: number = Number.parseInt(BC_CARS_PAGE_SIZE) || 8
+
+/**
+ * Bookings page size. Default is 8.
+ *
+ * @type {number}
+ */
 export const BOOKINGS_PAGE_SIZE: number = Number.parseInt(BC_BOOKINGS_PAGE_SIZE) || 8
+
+/**
+ * User images CDN.
+ *
+ * @type {string}
+ */
 export const CDN_USERS: string = BC_CDN_USERS
+
+/**
+ * Car images CDN.
+ *
+ * @type {string}
+ */
 export const CDN_CARS: string = BC_CDN_CARS
+
+/**
+ * Page offset.
+ *
+ * @type {number}
+ */
 export const PAGE_OFFSET: number = 200
+
+/**
+ * Supplier image width. Default is 60.
+ *
+ * @type {number}
+ */
 export const COMPANY_IMAGE_WIDTH: number = Number.parseInt(BC_COMPANY_IMAGE_WIDTH) || 60
+
+/**
+ * Supplier image height. Default is 30.
+ *
+ * @type {number}
+ */
 export const COMPANY_IMAGE_HEIGHT: number = Number.parseInt(BC_COMPANY_IMAGE_HEIGHT) || 30
+
+/**
+ * Car image width. Default is 300.
+ *
+ * @type {number}
+ */
 export const CAR_IMAGE_WIDTH: number = Number.parseInt(BC_CAR_IMAGE_WIDTH) || 300
+
+/**
+ * Car image height. Default is 200.
+ *
+ * @type {number}
+ */
 export const CAR_IMAGE_HEIGHT: number = Number.parseInt(BC_CAR_IMAGE_HEIGHT) || 200
-export const CAR_OPTION_IMAGE_HEIGHT: number = 85
-export const SELECTED_CAR_OPTION_IMAGE_HEIGHT: number = 30
+
+/**
+ * Minimum age. Default is 21.
+ *
+ * @type {number}
+ */
 export const MINIMUM_AGE: number = Number.parseInt(BC_MINIMUM_AGE) || 21
