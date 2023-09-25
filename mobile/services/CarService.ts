9b09bf0fe540313961826f45832cda18017921bc
aelassas <akram.elassas@gmail.com>
2023-09-25 16:28:57 +0100

    Add jsdoc to mobile services

--- mobile/services/CarService.ts
+++ mobile/services/CarService.ts
@@ -6,6 +6,15 @@ import * as bookcarsTypes from  '../miscellaneous/bookcarsTypes'
 
 AxiosHelper.init(axios)
 
+/**
+ * Get cars.
+ *
+ * @async
+ * @param {bookcarsTypes.GetCarsPayload} data
+ * @param {number} page
+ * @param {number} size
+ * @returns {Promise<bookcarsTypes.Result<bookcarsTypes.Car>>}
+ */
 export const getCars = async (data: bookcarsTypes.GetCarsPayload, page: number, size: number): Promise<bookcarsTypes.Result<bookcarsTypes.Car>> =>
   axios
     .post(
@@ -14,22 +23,18 @@ export const getCars = async (data: bookcarsTypes.GetCarsPayload, page: number, 
     )
     .then((res) => res.data)
 
+/**
+ * Get a Car by ID.
+ *
+ * @async
+ * @param {string} id
+ * @returns {Promise<bookcarsTypes.Car>}
+ */
 export const getCar = async (id: string): Promise<bookcarsTypes.Car> => {
   const language = await UserService.getLanguage()
   return axios
     .get(
       `${Env.API_HOST}/api/car/${encodeURIComponent(id)}/${language}`
-    )
-    .then((res) => res.data)
-}
-
-export const getBookingCars = async (keyword: string, data: bookcarsTypes.GetBookingCarsPayload, page: number, size: number): Promise<bookcarsTypes.Result<bookcarsTypes.Car>> => {
-  const authHeader = await UserService.authHeader()
-  return axios
-    .post(
-      `${Env.API_HOST}/api/booking-cars/${page}/${size}/?s=${encodeURIComponent(keyword)}`,
-      data,
-      { headers: authHeader }
     )
     .then((res) => res.data)
 }
