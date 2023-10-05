5f9c8b1debd38eb27d39e3e2cc4f0b2ea77749a2
aelassas <akram.elassas@gmail.com>
2023-10-05 07:43:06 +0100

    Update Checkbox.tsx

--- mobile/components/Checkbox.tsx
+++ mobile/components/Checkbox.tsx
@@ -3,25 +3,29 @@ import { Pressable, StyleSheet } from 'react-native'
 import { MaterialIcons } from '@expo/vector-icons'
 
 function Checkbox({
-    indeterminate,
-    checked,
-    onChange
-  }: {
-    indeterminate?: boolean
-    checked?: boolean
-    onChange?: (_checked: boolean) => void
-  }) {
+  indeterminate,
+  checked,
+  onChange
+}: {
+  indeterminate?: boolean
+  checked?: boolean
+  onChange?: (_checked: boolean) => void
+}) {
   return (
     <Pressable
       onPress={() => {
-      if (onChange) {
-        onChange(!checked)
-      }
-    }}
+        if (onChange) {
+          onChange(!checked)
+        }
+      }}
       hitSlop={15}
       style={styles.checkbox}
     >
-      <MaterialIcons name={indeterminate ? 'indeterminate-check-box' : checked ? 'check-box' : 'check-box-outline-blank'} size={24} color={indeterminate || checked ? '#1976d2' : '#606264'} />
+      <MaterialIcons
+        name={indeterminate ? 'indeterminate-check-box' : checked ? 'check-box' : 'check-box-outline-blank'}
+        size={24}
+        color={indeterminate || checked ? '#1976d2' : '#606264'}
+      />
     </Pressable>
   )
 }
