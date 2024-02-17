ea07fba5f4d38ec78a400eebb955cab50d3403e0
aelassas <akram.elassas@gmail.com>
2024-02-17 22:03:48 +0100

    Update user.test.ts

--- api/tests/user.test.ts
+++ api/tests/user.test.ts
@@ -50,4 +50,236 @@ describe('POST /api/create-user', () => {
     })
 })
 
-// TODO Add remaining tests
+describe('GET /api/check-token/:type/:userId/:email/:token', () => {
+    it("should check user's token", async () => {
+
+        // TODO
+
+    })
+})
+
+describe('DELETE /api/delete-tokens/:userId', () => {
+    it("should delete user's tokens", async () => {
+
+        // TODO
+
+    })
+})
+
+describe('POST /api/resend/:type/:email/:reset', () => {
+    it('should resend validation email', async () => {
+
+        // TODO
+
+    })
+})
+
+describe('POST /api/activate', () => {
+    it("should activate user's account", async () => {
+
+        // TODO
+
+    })
+})
+
+describe('POST /api/sign-in/:type', () => {
+    it('should sign in', async () => {
+
+        // TODO
+
+    })
+})
+
+describe('POST /api/sign-out', () => {
+    it('should sign out', async () => {
+
+        // TODO
+
+    })
+})
+
+describe('POST /api/create-push-token/:userId/:token', () => {
+    it('should create push token', async () => {
+        const token = await TestHelper.signinAsAdmin()
+
+        // TODO
+
+        await TestHelper.signout(token)
+    })
+})
+
+describe('GET /api/push-token/:userId', () => {
+    it('should get push token', async () => {
+        const token = await TestHelper.signinAsAdmin()
+
+        // TODO
+
+        await TestHelper.signout(token)
+    })
+})
+
+describe('POST /api/delete-push-token/:userId', () => {
+    it('should delete push token', async () => {
+        const token = await TestHelper.signinAsAdmin()
+
+        // TODO
+
+        await TestHelper.signout(token)
+    })
+})
+
+describe('POST /api/validate-email', () => {
+    it('should validate email', async () => {
+
+        // TODO
+
+    })
+})
+
+describe('POST /api/validate-access-token', () => {
+    it('should validate access token', async () => {
+        const token = await TestHelper.signinAsAdmin()
+
+        // TODO
+
+        await TestHelper.signout(token)
+    })
+})
+
+describe('POST /api/confirm-email/:email/:token', () => {
+    it('should send confirmation email', async () => {
+
+        // TODO
+
+    })
+})
+
+describe('POST /api/resend-link', () => {
+    it('should resend activation link', async () => {
+        const token = await TestHelper.signinAsAdmin()
+
+        // TODO
+
+        await TestHelper.signout(token)
+    })
+})
+
+describe('POST /api/update-user', () => {
+    it('should update user', async () => {
+        const token = await TestHelper.signinAsAdmin()
+
+        // TODO
+
+        await TestHelper.signout(token)
+    })
+})
+
+describe('POST /api/update-email-notifications', () => {
+    it('should update email notifications setting', async () => {
+        const token = await TestHelper.signinAsAdmin()
+
+        // TODO
+
+        await TestHelper.signout(token)
+    })
+})
+
+describe('POST /api/update-language', () => {
+    it("should update user's language", async () => {
+        const token = await TestHelper.signinAsAdmin()
+
+        // TODO
+
+        await TestHelper.signout(token)
+    })
+})
+
+describe('GET /api/user/:id', () => {
+    it('should get a user', async () => {
+        const token = await TestHelper.signinAsAdmin()
+
+        // TODO
+
+        await TestHelper.signout(token)
+    })
+})
+
+describe('POST /api/create-avatar', () => {
+    it("should create user's avatar", async () => {
+        const token = await TestHelper.signinAsAdmin()
+
+        // TODO
+
+        await TestHelper.signout(token)
+    })
+})
+
+describe('POST /api/update-avatar/:userId', () => {
+    it("should update user's avatar", async () => {
+        const token = await TestHelper.signinAsAdmin()
+
+        // TODO
+
+        await TestHelper.signout(token)
+    })
+})
+
+describe('POST /api/delete-avatar/:userId', () => {
+    it("should delete user's avatar", async () => {
+        const token = await TestHelper.signinAsAdmin()
+
+        // TODO
+
+        await TestHelper.signout(token)
+    })
+})
+
+describe('POST /api/delete-temp-avatar/:avatar', () => {
+    it('should delete temporary avatar', async () => {
+        const token = await TestHelper.signinAsAdmin()
+
+        // TODO
+
+        await TestHelper.signout(token)
+    })
+})
+
+describe('POST /api/change-password', () => {
+    it('should change password', async () => {
+        const token = await TestHelper.signinAsAdmin()
+
+        // TODO
+
+        await TestHelper.signout(token)
+    })
+})
+
+describe('GET /api/check-password/:id/:password', () => {
+    it('should check password', async () => {
+        const token = await TestHelper.signinAsAdmin()
+
+        // TODO
+
+        await TestHelper.signout(token)
+    })
+})
+
+describe('POST /api/users/:page/:size', () => {
+    it('should get users', async () => {
+        const token = await TestHelper.signinAsAdmin()
+
+        // TODO
+
+        await TestHelper.signout(token)
+    })
+})
+
+describe('POST /api/delete-users', () => {
+    it('should delete users', async () => {
+        const token = await TestHelper.signinAsAdmin()
+
+        // TODO
+
+        await TestHelper.signout(token)
+    })
+})
