0f1a131628608f5c2775bcf45baa65ec4eaffb0c
aelassas <akram.elassas@gmail.com>
2024-04-03 17:20:08 +0000

    Update helper.ts

--- backend/src/common/helper.ts
+++ backend/src/common/helper.ts
@@ -20,7 +20,7 @@ export const getLanguage = (code: string) => env._LANGUAGES.find((l) => l.code =
  * @param {string} message
  */
 export const info = (message: string) => {
-  toast(message, { type: 'info' })
+  toast.info(message)
 }
 
 /**
@@ -34,9 +34,9 @@ export const error = (err?: unknown, message?: string) => {
     console.error(err)
   }
   if (message) {
-    toast(message, { type: 'error' })
+    toast.error(message)
   } else {
-    toast(commonStrings.GENERIC_ERROR, { type: 'error' })
+    toast.error(commonStrings.GENERIC_ERROR)
   }
 }
 
