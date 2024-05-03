ba4c99577660b9c21d76d8816c69e8ddc96091d3
aelassas <akram.elassas@gmail.com>
2024-05-03 18:54:28 +0100

    Update mailHelper.ts

--- api/src/common/mailHelper.ts
+++ api/src/common/mailHelper.ts
@@ -9,25 +9,25 @@ import * as env from '../config/env.config'
  * @param {nodemailer.SendMailOptions} mailOptions
  * @returns {Promise<unknown>}
  */
-export const sendMail = (mailOptions: nodemailer.SendMailOptions) => {
-    const transporterOptions: SMTPTransport.Options = {
-        host: env.SMTP_HOST,
-        port: env.SMTP_PORT,
-        auth: {
-            user: env.SMTP_USER,
-            pass: env.SMTP_PASS,
-        },
-    }
+export const sendMail = (mailOptions: nodemailer.SendMailOptions): Promise<nodemailer.SentMessageInfo> => {
+  const transporterOptions: SMTPTransport.Options = {
+    host: env.SMTP_HOST,
+    port: env.SMTP_PORT,
+    auth: {
+      user: env.SMTP_USER,
+      pass: env.SMTP_PASS,
+    },
+  }
 
-    const transporter: nodemailer.Transporter = nodemailer.createTransport(transporterOptions)
+  const transporter: nodemailer.Transporter = nodemailer.createTransport(transporterOptions)
 
-    return new Promise((resolve, reject) => {
-        transporter.sendMail(mailOptions, (err: Error | null, info: nodemailer.SentMessageInfo) => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(info)
-            }
-        })
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (err: Error | null, info: nodemailer.SentMessageInfo) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(info)
+      }
     })
+  })
 }
