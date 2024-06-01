cf11b6f1aab79d2c0eb01a1bf8dbec06ebae197f
aelassas <akram.elassas@gmail.com>
2024-06-01 19:25:38 +0100

    Update frontend layout

--- backend/src/App.tsx
+++ backend/src/App.tsx
@@ -47,21 +47,21 @@ const App = () => (
             <Route path="/" element={<Bookings />} />
             <Route path="/suppliers" element={<Suppliers />} />
             <Route path="/supplier" element={<Supplier />} />
-            <Route path="/create-supplier" element={<CreateSupplier />} />
-            <Route path="/update-supplier" element={<UpdateSupplier />} />
+            {/* <Route path="/create-supplier" element={<CreateSupplier />} /> */}
+            {/* <Route path="/update-supplier" element={<UpdateSupplier />} /> */}
             <Route path="/locations" element={<Locations />} />
             <Route path="/create-location" element={<CreateLocation />} />
             <Route path="/update-location" element={<UpdateLocation />} />
             <Route path="/cars" element={<Cars />} />
             <Route path="/car" element={<Car />} />
-            <Route path="/create-car" element={<CreateCar />} />
-            <Route path="/update-car" element={<UpdateCar />} />
+            {/* <Route path="/create-car" element={<CreateCar />} /> */}
+            {/* <Route path="/update-car" element={<UpdateCar />} /> */}
             <Route path="/update-booking" element={<UpdateBooking />} />
             <Route path="/create-booking" element={<CreateBooking />} />
             <Route path="/users" element={<Users />} />
             <Route path="/user" element={<User />} />
-            <Route path="/create-user" element={<CreateUser />} />
-            <Route path="/update-user" element={<UpdateUser />} />
+            {/* <Route path="/create-user" element={<CreateUser />} /> */}
+            {/* <Route path="/update-user" element={<UpdateUser />} /> */}
             <Route path="/settings" element={<Settings />} />
             <Route path="/notifications" element={<Notifications />} />
             {/* <Route path="/change-password" element={<ChangePassword />} /> */}
