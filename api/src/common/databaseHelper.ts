8e7cce3a596d824d26c109d39b5b694644bc7d3b
aelassas <akram.elassas@gmail.com>
2024-06-21 23:56:43 +0100

    Update databaseHelper.ts

--- api/src/common/databaseHelper.ts
+++ api/src/common/databaseHelper.ts
@@ -181,15 +181,15 @@ export const InitializeLocations = async () => {
     }
 
     // Delete LocationValue nin env.LANGUAGES
-    // const values = await LocationValue.find({ language: { $in: env.LANGUAGES } })
-    // for (const val of values) {
-    //   const _locations = await Location.find({ values: val._id })
-    //   for (const _loc of _locations) {
-    //     _loc.values.splice(_loc.values.findIndex((v) => v.equals(val.id)), 1)
-    //     await _loc.save()
-    //   }
-    // }
-    // await LocationValue.deleteMany({ language: { $in: env.LANGUAGES } })
+    const values = await LocationValue.find({ language: { $nin: env.LANGUAGES } })
+    for (const val of values) {
+      const _locations = await Location.find({ values: val._id })
+      for (const _loc of _locations) {
+        _loc.values.splice(_loc.values.findIndex((v) => v.equals(val.id)), 1)
+        await _loc.save()
+      }
+    }
+    await LocationValue.deleteMany({ language: { $nin: env.LANGUAGES } })
 
     logger.info('Locations initialized')
     return true
