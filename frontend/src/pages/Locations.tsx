fe025790d04adb93221e731431a03648a6e34318
aelassas <akram.elassas@gmail.com>
2024-08-05 12:01:14 +0100

    Update Locations.tsx

--- frontend/src/pages/Locations.tsx
+++ frontend/src/pages/Locations.tsx
@@ -1,19 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Dialog, DialogContent } from '@mui/material'
+import L from 'leaflet'
+import env from '../config/env.config'
+import * as bookcarsTypes from ':bookcars-types'
+import * as bookcarsHelper from ':bookcars-helper'
+import * as LocationService from '../services/LocationService'
 import Layout from '../components/Layout'
 import Map from '../components/Map'
+import SearchForm from '../components/SearchForm'
 import Footer from '../components/Footer'
 
 import '../assets/css/locations.css'
 
 const Locations = () => {
-  const onLoad = () => {
+  const [locations, setLocations] = useState<bookcarsTypes.Location[]>([])
+  const [pickupLocation, setPickupLocation] = useState('')
+  const [openSearchFormDialog, setOpenSearchFormDialog] = useState(false)
+
+  const onLoad = async () => {
+    const _locations = await LocationService.getLocationsWithPosition()
+    setLocations(_locations)
   }
 
   return (
     <Layout onLoad={onLoad} strict={false}>
       <div className="locations">
-        <Map className="map" initialZoom={4.5} />
+        <Map
+          position={new L.LatLng(34.0268755, 1.6528399999999976)}
+          initialZoom={5}
+          locations={locations}
+          onSelelectPickUpLocation={async (locationId) => {
+            setPickupLocation(locationId)
+            setOpenSearchFormDialog(true)
+          }}
+        />
       </div>
+
+      <Dialog
+        fullWidth={env.isMobile()}
+        maxWidth={false}
+        open={openSearchFormDialog}
+        onClose={() => {
+          setOpenSearchFormDialog(false)
+        }}
+      >
+        <DialogContent className="search-dialog-content">
+          <SearchForm
+            ranges={bookcarsHelper.getAllRanges()}
+            pickupLocation={pickupLocation}
+            onCancel={() => {
+              setOpenSearchFormDialog(false)
+            }}
+          />
+        </DialogContent>
+      </Dialog>
+
       <Footer />
     </Layout>
   )
