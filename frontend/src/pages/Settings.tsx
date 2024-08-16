9c2511d9d9cf6ebeb24b5fa7f32f740364261bc1
aelassas <akram.elassas@gmail.com>
2024-08-16 12:42:58 +0100

    Restrict avatar edit

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -184,7 +184,8 @@ const Settings = () => {
                 loggedUser={user}
                 user={user}
                 size="large"
-                readonly={false}
+                // readonly={false}
+                readonly
                 onBeforeUpload={onBeforeUpload}
                 onChange={onAvatarChange}
                 color="disabled"
