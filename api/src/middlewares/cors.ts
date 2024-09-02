d51879be5c3d519cc499ee79691e22545c0a52ef
aelassas <akram.elassas@gmail.com>
2024-09-02 16:41:53 +0100

    Update cors middleware

--- api/src/middlewares/cors.ts
+++ api/src/middlewares/cors.ts
@@ -1,4 +1,12 @@
 import cors from 'cors'
+import * as helper from '../common/helper'
+import * as env from '../config/env.config'
+import * as logger from '../common/logger'
+
+const whitelist = [
+  helper.trimEnd(env.BACKEND_HOST, '/'),
+  helper.trimEnd(env.FRONTEND_HOST, '/'),
+]
 
 /**
  * CORS configuration.
@@ -6,8 +14,17 @@ import cors from 'cors'
  * @type {cors.CorsOptions}
  */
 const CORS_CONFIG: cors.CorsOptions = {
-    origin: true,
-    credentials: true,
+  origin(origin, callback) {
+    if (!origin || whitelist.indexOf(origin) > -1) {
+      callback(null, true)
+    } else {
+      const message = `Not allowed by CORS: ${origin}`
+      logger.error(message)
+      callback(new Error(message))
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
 /**
