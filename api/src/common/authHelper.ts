b8486d4be457ed2deae68897fe57bc7bcba32ef7
aelassas <akram.elassas@gmail.com>
2024-09-16 20:33:35 +0100

    Replace jsonwebtoken by jose

--- api/src/common/authHelper.ts
+++ api/src/common/authHelper.ts
@@ -1,6 +1,44 @@
 import { Request } from 'express'
+import { JWTPayload, jwtVerify, SignJWT } from 'jose'
 import * as helper from './helper'
 import * as env from '../config/env.config'
+
+const jwtSecret = new TextEncoder().encode(env.JWT_SECRET)
+const jwtAlg = 'HS256'
+
+/**
+ * Sign and return the JWT.
+ *
+ * @async
+ * @param {JWTPayload} payload
+ * @param {?boolean} [stayConnected]
+ * @returns {Promise<string>}
+ */
+export const encryptJWT = async (payload: JWTPayload, stayConnected?: boolean) => {
+  const jwt = await new SignJWT(payload)
+    .setProtectedHeader({ alg: jwtAlg })
+    .setIssuedAt()
+
+  if (!stayConnected) {
+    jwt.setExpirationTime(`${env.JWT_EXPIRE_AT} seconds`)
+  }
+
+  return jwt.sign(jwtSecret)
+}
+
+/**
+ * Verify the JWT format, verify the JWS signature, validate the JWT Claims Set.
+ *
+ * @async
+ * @param {string} input
+ * @returns {Promise<JWTPayload>}
+ */
+export const decryptJWT = async (input: string) => {
+  const { payload } = await jwtVerify(input, jwtSecret, {
+    algorithms: [jwtAlg],
+  })
+  return payload
+}
 
 /**
  * Check whether the request is from the backend or not.
@@ -27,16 +65,16 @@ export const isFrontend = (req: Request): boolean => !!req.headers.origin && hel
  * @returns {string}
  */
 export const getAuthCookieName = (req: Request): string => {
-    if (isBackend(req)) {
-        // Backend auth cookie name
-        return env.BACKEND_AUTH_COOKIE_NAME
-    }
+  if (isBackend(req)) {
+    // Backend auth cookie name
+    return env.BACKEND_AUTH_COOKIE_NAME
+  }
 
-    if (isFrontend(req)) {
-        // Frontend auth cookie name
-        return env.FRONTEND_AUTH_COOKIE_NAME
-    }
+  if (isFrontend(req)) {
+    // Frontend auth cookie name
+    return env.FRONTEND_AUTH_COOKIE_NAME
+  }
 
-    // Mobile app and unit tests auth header name
-    return env.X_ACCESS_TOKEN
+  // Mobile app and unit tests auth header name
+  return env.X_ACCESS_TOKEN
 }
