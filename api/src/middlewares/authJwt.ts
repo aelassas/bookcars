61ba9b0f96e0300dc4d87bcdb6c891a8309ea26d
aelassas <akram.elassas@gmail.com>
2024-09-17 07:44:39 +0100

    Update authJwt.ts

--- api/src/middlewares/authJwt.ts
+++ api/src/middlewares/authJwt.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
+import mongoose from 'mongoose'
+import * as bookcarsTypes from ':bookcars-types'
 import * as env from '../config/env.config'
 import * as helper from '../common/helper'
 import * as authHelper from '../common/authHelper'
@@ -14,10 +16,12 @@ import User from '../models/User'
  */
 const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
   let token: string
+  const isBackend = authHelper.isBackend(req)
+  const isFrontend = authHelper.isFrontend(req)
 
-  if (authHelper.isBackend(req)) {
+  if (isBackend) {
     token = req.signedCookies[env.BACKEND_AUTH_COOKIE_NAME] as string // backend
-  } else if (authHelper.isFrontend(req)) {
+  } else if (isFrontend) {
     token = req.signedCookies[env.FRONTEND_AUTH_COOKIE_NAME] as string // frontend
   } else {
     token = req.headers[env.X_ACCESS_TOKEN] as string // mobile app and unit tests
@@ -27,11 +31,23 @@ const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
     // Check token
     try {
       const sessionData = await authHelper.decryptJWT(token)
+      const $match: mongoose.FilterQuery<any> = {
+        $and: [
+          { _id: sessionData?.id },
+          { blacklisted: false },
+        ],
+      }
+
+      if (isBackend) {
+        $match.$and?.push({ type: { $in: [bookcarsTypes.UserType.Admin, bookcarsTypes.UserType.Supplier] } })
+      } else if (isFrontend) {
+        $match.$and?.push({ type: bookcarsTypes.UserType.User })
+      }
 
       if (
         !sessionData
         || !helper.isValidObjectId(sessionData.id)
-        || !(await User.exists({ _id: sessionData.id, blacklisted: false }))
+        || !(await User.exists($match))
       ) {
         // Token not valid!
         logger.info('Token not valid: User not found')
