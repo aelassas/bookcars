c987b4e1cdddca57a23ce71c8a0a9732266a09cc
aelassas <akram.elassas@gmail.com>
2024-10-27 14:04:36 +0100

    Update build command

---
+++ __scripts/swap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SWAP_SIZE="1G"  # Swap size of 1GB
+
+# Add Swap Space
+echo "Adding swap space..."
+sudo fallocate -l $SWAP_SIZE /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
+# Make swap permanent
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+grep '^Swap' /proc/meminfo
