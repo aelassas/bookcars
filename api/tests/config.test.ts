e0993652acf3ce92fa01d4ba6457dd51e34b726b
aelassas <akram.elassas@gmail.com>
2024-11-19 15:00:11 +0100

    Add config.test.ts comments

--- api/tests/config.test.ts
+++ api/tests/config.test.ts
@@ -3,9 +3,11 @@ import * as env from '../src/config/env.config'
 
 describe('Test configuration options', () => {
     it('should test configuration options', async () => {
+      // test success (found and required)
         const SMTP_HOST = env.__env__('BC_SMTP_HOST', true)
         expect(!!SMTP_HOST).toBeTruthy()
 
+        // test failure (not found and required)
         let res = true
         try {
             const UNKNOWN = env.__env__('BC_UNKNOWN', true)
@@ -15,9 +17,11 @@ describe('Test configuration options', () => {
         }
         expect(res).toBeFalsy()
 
+        // test success (found and optional)
         const PORT = env.__env__('BC_PORT', false, '4002')
         expect(!!PORT).toBeTruthy()
 
+        // test success (not found and optional)
         const UNKNOWN = env.__env__('BC_UNKNOWN', false)
         expect(!!UNKNOWN).toBeFalsy()
     })
