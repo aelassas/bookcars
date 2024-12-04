07e2fb1b5bae65e7a82e2091bdcd8fde12b2ae7a
aelassas <akram.elassas@gmail.com>
2024-12-04 08:34:43 +0100

    Update databaseHelper.ts

--- api/__tests__/database.test.ts
+++ api/__tests__/database.test.ts
@@ -86,3 +86,11 @@ describe('Test database initialization', () => {
     await databaseHelper.close()
   })
 })
+
+describe('Test database initialization failures', () => {
+  it('should check database initialization failures', async () => {
+    expect(await databaseHelper.initializeLocations()).toBeFalsy()
+    expect(await databaseHelper.initializeCountries()).toBeFalsy()
+    expect(await databaseHelper.initializeParkingSpots()).toBeFalsy()
+  })
+})
