29c6b5a3c8f5237ddb7e5e3fa01c804c8c76c0c4
aelassas <akram.elassas@gmail.com>
2024-12-09 13:36:54 +0100

    Add CheckoutStatus.tsx

--- api/src/controllers/bookingController.ts
+++ api/src/controllers/bookingController.ts
@@ -744,6 +744,32 @@ export const getBooking = async (req: Request, res: Response) => {
 }
 
 /**
+ * Get Booking by sessionId.
+ *
+ * @export
+ * @async
+ * @param {Request} req
+ * @param {Response} res
+ * @returns {unknown}
+ */
+export const getBookingId = async (req: Request, res: Response) => {
+  const { sessionId } = req.params
+
+  try {
+    const booking = await Booking.findOne({ sessionId })
+
+    if (!booking) {
+      logger.error('[booking.getBookingId] Booking not found (sessionId):', sessionId)
+      return res.sendStatus(204)
+    }
+    return res.json(booking?.id)
+  } catch (err) {
+    logger.error(`[booking.getBookingId] (sessionId) ${i18n.t('DB_ERROR')} ${sessionId}`, err)
+    return res.status(400).send(i18n.t('DB_ERROR') + err)
+  }
+}
+
+/**
  * Get Bookings.
  *
  * @export
---
+++ frontend/src/components/Toast.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import {
+  CheckCircle as CheckIcon,
+  Error as ErrorIcon,
+  Close as CloseIcon
+} from '@mui/icons-material'
+
+import '@/assets/css/toast.css'
+
+interface ToastProps {
+  title?: string,
+  text: string
+  status: 'success' | 'error'
+}
+
+const Toast = ({ title, text, status }: ToastProps) => {
+  const [close, setClose] = useState(false)
+  return (
+    !close && (
+      <div className={`toast ${status}`}>
+        {status === 'success' ? <CheckIcon className="icon" /> : <ErrorIcon className="icon" />}
+        <div className="toast-content">
+          {title && <span className="title">{title}</span>}
+          <span className="text">{text}</span>
+        </div>
+        <div onClick={() => setClose(true)} role="presentation" className="close">
+          <CloseIcon className="icon" />
+        </div>
+      </div>
+    )
+  )
+}
+
+export default Toast
