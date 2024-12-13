9af603259bde090f03ac26446cc98ee8e44e0476
aelassas <akram.elassas@gmail.com>
2024-12-13 08:37:31 +0100

    Add show filters button to search

--- frontend/src/lang/search.ts
+++ frontend/src/lang/search.ts
@@ -4,12 +4,18 @@ import * as langHelper from '@/common/langHelper'
 const strings = new LocalizedStrings({
   fr: {
     VIEW_ON_MAP: 'Voir sur la carte',
+    SHOW_FILTERS: 'Afficher les filtres',
+    HILE_FILTERS: 'Masquer les filtres',
   },
   en: {
     VIEW_ON_MAP: 'View on map',
+    SHOW_FILTERS: 'Show Filters',
+    HILE_FILTERS: 'Hide Filters',
   },
   es: {
     VIEW_ON_MAP: 'Ver en el mapa',
+    SHOW_FILTERS: 'Mostrar filtros',
+    HILE_FILTERS: 'Ocultar filtros',
   },
 })
 
