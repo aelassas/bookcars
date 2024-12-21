5a4fa5c45064ab2e5bd67d5bbca58a04b454f242
aelassas <akram.elassas@gmail.com>
2024-12-21 09:26:11 +0100

    Fix SuspenseRouter.tsx

--- frontend/src/components/SuspenseRouter.tsx
+++ frontend/src/components/SuspenseRouter.tsx
@@ -10,7 +10,6 @@ export interface BrowserRouterProps {
 
 export const SuspenseRouter = ({ basename, children, window }: BrowserRouterProps) => {
   const historyRef = useRef<BrowserHistory>(null)
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [isPending, startTransition] = useTransition()
 
   if (historyRef.current == null) {
