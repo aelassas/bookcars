b56b76895a16c10947c2594c298d02927ec3cf6c
aelassas <akram.elassas@gmail.com>
2025-01-14 21:12:15 +0100

    Fix pull request #61

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,7 @@
 import path from 'node:path'
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import { createHtmlPlugin } from 'vite-plugin-html';
-
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default ({ mode }: { mode: string }) => {
