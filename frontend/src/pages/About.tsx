197d43540e731d838302fd8e58dd20b0d80df321
aelassas <akram.elassas@gmail.com>
2025-01-15 09:30:52 +0100

    Add explicit text to about page

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,19 +1,39 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Button } from '@mui/material'
+import { strings } from '@/lang/about'
 import Layout from '@/components/Layout'
+import Footer from '@/components/Footer'
 
 import '@/assets/css/about.css'
 
 const About = () => {
+  const navigate = useNavigate()
+
   const onLoad = () => { }
 
   return (
     <Layout onLoad={onLoad} strict={false}>
       <div className="about">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-        in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-        sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <h1>{strings.TITLE1}</h1>
+        <h2>{strings.SUBTITLE1}</h2>
+        <p>{strings.CONTENT1}</p>
+
+        <h1>{strings.TITLE2}</h1>
+        <h2>{strings.SUBTITLE2}</h2>
+        <p>{strings.CONTENT2}</p>
+
+        <Button
+          variant="contained"
+          className="btn-primary"
+          aria-label="Find deal"
+          onClick={() => navigate('/')}
+        >
+          {strings.FIND_DEAL}
+        </Button>
       </div>
+
+      <Footer />
     </Layout>
   )
 }
