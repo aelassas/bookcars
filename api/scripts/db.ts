12afb20621a4b4218c558346466faa95ecd982ef
aelassas <akram.elassas@gmail.com>
2025-01-21 15:04:31 +0100

    Rename db:script to script:db

--- api/scripts/db.ts
+++ api/scripts/db.ts
@@ -1,12 +1,12 @@
 import 'dotenv/config'
-import * as bookcarsTypes from ':bookcars-types'
+// import * as bookcarsTypes from ':bookcars-types'
 import * as env from '../src/config/env.config'
 import * as logger from '../src/common/logger'
 import * as databaseHelper from '../src/common/databaseHelper'
-import User from '../src/models/User'
+// import User from '../src/models/User'
 
 if (await databaseHelper.connect(env.DB_URI, env.DB_SSL, env.DB_DEBUG)) {
-  await User.updateMany({ type: bookcarsTypes.UserType.Supplier }, { payLater: true })
+  // await User.updateMany({ type: bookcarsTypes.UserType.Supplier }, { payLater: true })
 
   await databaseHelper.close()
   logger.info('Database connection closed')
