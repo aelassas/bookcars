1bd3b11a600f998d6b0463bdf46c92f39085f22e
aelassas <akram.elassas@gmail.com>
2025-02-01 20:08:07 +0100

    Add PayPal sandbox mode configuration

--- api/src/config/env.config.ts
+++ api/src/config/env.config.ts
@@ -336,6 +336,13 @@ stripeSessionExpireAt = stripeSessionExpireAt <= 82800 ? stripeSessionExpireAt :
 export const STRIPE_SESSION_EXPIRE_AT = stripeSessionExpireAt
 
 /**
+ * Indicates whether PayPal is used in sandbox mode or production.
+ *
+ * @type {boolean}
+ */
+export const PAYPAL_SANDBOX = helper.StringToBoolean(__env__('BC_PAYPAL_SANDBOX', false, 'true'))
+
+/**
  * PayPal client ID.
  *
  * @type {string}
