618cfc7ab1a0c1c011affb0bdec315fcccdc442c
aelassas <akram.elassas@gmail.com>
2025-02-08 20:20:36 +0100

    Add total records header to search page

--- mobile/components/CarList.tsx
+++ mobile/components/CarList.tsx
@@ -1,181 +1,195 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, ActivityIndicator, RefreshControl } from 'react-native'
-import { KeyboardAwareFlatList } from 'react-native-keyboard-aware-scroll-view'
-import type { NativeStackNavigationProp } from '@react-navigation/native-stack'
-import { CommonActions, NavigationRoute, RouteProp } from '@react-navigation/native'
+import {
+  Card,
+  CardContent,
+  Typography
+} from '@mui/material'
 import * as bookcarsTypes from ':bookcars-types'
 import * as bookcarsHelper from ':bookcars-helper'
-
+import Const from '@/config/const'
+import env from '@/config/env.config'
 import * as helper from '@/common/helper'
-import * as env from '@/config/env.config'
-import i18n from '@/lang/i18n'
-import * as UserService from '@/services/UserService'
+import { strings } from '@/lang/cars'
 import * as CarService from '@/services/CarService'
-import Car from './Car'
+import Pager from '@/components/Pager'
+import Car from '@/components/Car'
+import Progress from '@/components/Progress'
+
+import '@/assets/css/car-list.css'
 
 interface CarListProps {
-  navigation: NativeStackNavigationProp<StackParams, keyof StackParams>
   from?: Date
   to?: Date
   suppliers?: string[]
-  rating?: number
-  ranges?: bookcarsTypes.CarRange[]
-  multimedia?: bookcarsTypes.CarMultimedia[]
-  seats?: number,
-  carSpecs?: bookcarsTypes.CarSpecs,
   pickupLocation?: string
   dropOffLocation?: string
   pickupLocationName?: string
-  distance?: string
+  carSpecs?: bookcarsTypes.CarSpecs
   carType?: string[]
   gearbox?: string[]
   mileage?: string[]
   fuelPolicy?: string[]
   deposit?: number
-  header?: React.ReactElement
   cars?: bookcarsTypes.Car[]
+  reload?: boolean
+  booking?: bookcarsTypes.Booking
+  className?: string
   hidePrice?: boolean
-  footerComponent?: React.ReactElement
-  routeName?: 'Cars' | 'Checkout',
-  route: RouteProp<StackParams, keyof StackParams>
+  hideSupplier?: boolean
+  loading?: boolean
+  sizeAuto?: boolean
+  ranges?: string[]
+  multimedia?: string[]
+  rating?: number
+  seats?: number
+  distance?: string
   includeAlreadyBookedCars?: boolean
   includeComingSoonCars?: boolean
   onLoad?: bookcarsTypes.DataEvent<bookcarsTypes.Car>
 }
 
 const CarList = ({
-  navigation,
   from,
   to,
   suppliers,
-  rating,
-  ranges,
-  multimedia,
-  seats,
-  carSpecs,
   pickupLocation,
   dropOffLocation,
   pickupLocationName,
-  distance,
+  carSpecs,
   carType: _carType,
   gearbox,
   mileage,
   fuelPolicy,
   deposit,
-  header,
   cars,
+  reload,
+  booking,
+  className,
   hidePrice,
-  footerComponent,
-  routeName,
-  // route,
+  hideSupplier,
+  loading: carListLoading,
+  sizeAuto,
+  ranges,
+  multimedia,
+  rating,
+  seats,
+  distance,
   includeAlreadyBookedCars,
   includeComingSoonCars,
   onLoad,
 }: CarListProps) => {
-  const [language, setLanguage] = useState(env.DEFAULT_LANGUAGE)
-  const [onScrollEnd, setOnScrollEnd] = useState(false)
-  const [loading, setLoading] = useState(true)
+  const [init, setInit] = useState(true)
+  const [loading, setLoading] = useState(false)
   const [fetch, setFetch] = useState(false)
   const [rows, setRows] = useState<bookcarsTypes.Car[]>([])
+  const [rowCount, setRowCount] = useState(0)
+  const [totalRecords, setTotalRecords] = useState(0)
   const [page, setPage] = useState(1)
-  const [refreshing, setRefreshing] = useState(false)
 
   useEffect(() => {
-    const init = async () => {
-      try {
-        const _language = await UserService.getLanguage()
-        i18n.locale = _language
-        setLanguage(_language)
-      } catch (err) {
-        helper.error(err)
+    if (env.PAGINATION_MODE === Const.PAGINATION_MODE.INFINITE_SCROLL || env.isMobile) {
+      const element = document.querySelector('body')
+
+      if (element) {
+        element.onscroll = () => {
+          if (fetch
+            && !loading
+            && window.scrollY > 0
+            && window.scrollY + window.innerHeight + env.INFINITE_SCROLL_OFFSET >= document.body.scrollHeight) {
+            setLoading(true)
+            setPage(page + 1)
+          }
+        }
       }
     }
-
-    init()
-  }, [])
+  }, [fetch, loading, page])
 
   const fetchData = async (
     _page: number,
     _suppliers?: string[],
-    _rating?: number,
-    _ranges?: bookcarsTypes.CarRange[],
-    _multimedia?: bookcarsTypes.CarMultimedia[],
-    _seats?: number,
-    _carSpecs?: bookcarsTypes.CarSpecs,
     _pickupLocation?: string,
+    _carSpecs?: bookcarsTypes.CarSpecs,
     __carType?: string[],
     _gearbox?: string[],
     _mileage?: string[],
     _fuelPolicy?: string[],
-    _deposit?: number
+    _deposit?: number,
+    _ranges?: string[],
+    _multimedia?: string[],
+    _rating?: number,
+    _seats?: number,
   ) => {
     try {
-      if (_suppliers && _suppliers.length > 0) {
-        setLoading(true)
-        setFetch(true)
+      setLoading(true)
 
-        const payload: bookcarsTypes.GetCarsPayload = {
-          suppliers: _suppliers,
-          rating: _rating,
-          ranges: _ranges,
-          multimedia: _multimedia,
-          seats: _seats,
-          carSpecs: _carSpecs,
-          pickupLocation: _pickupLocation,
-          carType: __carType,
-          gearbox: _gearbox,
-          mileage: _mileage,
-          fuelPolicy: _fuelPolicy,
-          deposit: _deposit,
-          includeAlreadyBookedCars,
-          includeComingSoonCars,
-          days: bookcarsHelper.days(from, to),
-        }
+      const payload: bookcarsTypes.GetCarsPayload = {
+        suppliers: _suppliers ?? [],
+        pickupLocation: _pickupLocation,
+        carSpecs: _carSpecs,
+        carType: __carType,
+        gearbox: _gearbox,
+        mileage: _mileage,
+        fuelPolicy: _fuelPolicy,
+        deposit: _deposit,
+        ranges: _ranges,
+        multimedia: _multimedia,
+        rating: _rating,
+        seats: _seats,
+        days: bookcarsHelper.days(from, to),
+        includeAlreadyBookedCars,
+        includeComingSoonCars,
+      }
 
-        const data = await CarService.getCars(payload, _page, env.CARS_PAGE_SIZE)
-        const _data = data && data.length > 0 ? data[0] : { pageInfo: { totalRecord: 0 }, resultData: [] }
-        if (!_data) {
-          helper.error()
-          return
-        }
-        const totalRecords = Array.isArray(_data.pageInfo) && _data.pageInfo.length > 0 ? _data.pageInfo[0].totalRecords : 0
-        const _rows = _page === 1 ? _data.resultData : [...rows, ..._data.resultData]
+      const data = await CarService.getCars(payload, _page, env.CARS_PAGE_SIZE)
 
-        setRows(_rows)
-        setFetch(_data.resultData.length === env.CARS_PAGE_SIZE)
-        if (onLoad) {
-          onLoad({ rows: _data.resultData, rowCount: totalRecords })
-        }
+      const _data = data && data.length > 0 ? data[0] : { pageInfo: { totalRecord: 0 }, resultData: [] }
+      if (!_data) {
+        helper.error()
+        return
+      }
+      const _totalRecords = Array.isArray(_data.pageInfo) && _data.pageInfo.length > 0 ? _data.pageInfo[0].totalRecords : 0
+
+      let _rows = []
+      if (env.PAGINATION_MODE === Const.PAGINATION_MODE.INFINITE_SCROLL || env.isMobile) {
+        _rows = _page === 1 ? _data.resultData : [...rows, ..._data.resultData]
       } else {
-        setRows([])
-        setFetch(false)
+        _rows = _data.resultData
+      }
+
+      setRows(_rows)
+      setRowCount((_page - 1) * env.CARS_PAGE_SIZE + _rows.length)
+      setTotalRecords(_totalRecords)
+      setFetch(_data.resultData.length > 0)
+
+      if (((env.PAGINATION_MODE === Const.PAGINATION_MODE.INFINITE_SCROLL || env.isMobile) && _page === 1) || (env.PAGINATION_MODE === Const.PAGINATION_MODE.CLASSIC && !env.isMobile)) {
+        window.scrollTo(0, 0)
+      }
+
+      if (onLoad) {
+        onLoad({ rows: _data.resultData, rowCount: _totalRecords })
       }
     } catch (err) {
       helper.error(err)
     } finally {
       setLoading(false)
+      setInit(false)
     }
   }
 
   useEffect(() => {
     if (suppliers) {
-      if (suppliers && rating && ranges && multimedia && seats && carSpecs && pickupLocation && _carType && gearbox && mileage && fuelPolicy && deposit) {
-        fetchData(page, suppliers, rating, ranges, multimedia, seats, carSpecs, pickupLocation, _carType, gearbox, mileage, fuelPolicy, deposit)
+      if (suppliers.length > 0) {
+        fetchData(page, suppliers, pickupLocation, carSpecs, _carType, gearbox, mileage, fuelPolicy, deposit, ranges, multimedia, rating, seats)
       } else {
         setRows([])
         setFetch(false)
         if (onLoad) {
           onLoad({ rows: [], rowCount: 0 })
         }
+        setInit(false)
       }
-    } // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [page, suppliers, rating, ranges, multimedia, seats, carSpecs, pickupLocation, _carType, gearbox, mileage, fuelPolicy, deposit])
-
-  useEffect(() => {
-    if (suppliers && rating && ranges && multimedia && seats && carSpecs && pickupLocation && _carType && gearbox && mileage && fuelPolicy && deposit) {
-      setPage(1)
     }
-  }, [suppliers, rating, ranges, multimedia, seats, carSpecs, pickupLocation, _carType, gearbox, mileage, fuelPolicy, deposit])
+  }, [page, suppliers, pickupLocation, carSpecs, _carType, gearbox, mileage, fuelPolicy, deposit, ranges, multimedia, rating, seats, from, to]) // eslint-disable-line react-hooks/exhaustive-deps
 
   useEffect(() => {
     if (cars) {
@@ -188,175 +202,72 @@ const CarList = ({
     }
   }, [cars]) // eslint-disable-line react-hooks/exhaustive-deps
 
-  const numToRender = Math.floor(env.CARS_PAGE_SIZE / 2)
+  useEffect(() => {
+    setPage(1)
+  }, [suppliers, pickupLocation, carSpecs, _carType, gearbox, mileage, fuelPolicy, deposit, ranges, multimedia, rating, seats, from, to])
+
+  useEffect(() => {
+    if (reload) {
+      setPage(1)
+      fetchData(1, suppliers, pickupLocation, carSpecs, _carType, gearbox, mileage, fuelPolicy, deposit, ranges, multimedia, rating, seats)
+    }
+  }, [reload, suppliers, pickupLocation, carSpecs, _carType, gearbox, mileage, fuelPolicy, deposit, ranges, multimedia, rating, seats]) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <View style={styles.container}>
-      {((from && to && pickupLocation && dropOffLocation) || hidePrice) && (
-        <KeyboardAwareFlatList
+    <>
+      <section className={`${className ? `${className} ` : ''}car-list`}>
+        {rows.length === 0
+          ? !init
+          && !loading
+          && !carListLoading
+          && (
+            <Card variant="outlined" className="empty-list">
+              <CardContent>
+                <Typography color="textSecondary">{strings.EMPTY_LIST}</Typography>
+              </CardContent>
+            </Card>
+          )
+          : ((from && to && pickupLocation && dropOffLocation) || hidePrice) // || (hidePrice && booking))
+          && (
+            <>
+              {totalRecords > 0 && (
+                <div className="title">
+                  <div className="bookcars">
+                    <span>{strings.TITLE_1}</span>
+                    <span className="title-bookcars">{env.WEBSITE_NAME}</span>
+                    <span>{strings.TITLE_2}</span>
+                  </div>
+                  <div className="car-count">
+                    {`(${totalRecords} ${totalRecords === 1 ? strings.TITLE_CAR_AVAILABLE : strings.TITLE_CARS_AVAILABLE})`}
+                  </div>
+                </div>
+              )}
 
-          automaticallyAdjustKeyboardInsets
-          keyboardShouldPersistTaps={helper.android() ? 'handled' : 'always'}
-
-          extraHeight={20}
-          extraScrollHeight={20}
-          enableOnAndroid
-
-          initialNumToRender={numToRender}
-          maxToRenderPerBatch={numToRender}
-          removeClippedSubviews
-          contentContainerStyle={styles.contentContainer}
-          style={styles.flatList}
-          data={rows}
-          renderItem={({ item: car }) => (
-            <Car
-              car={car}
-              language={language}
-              from={from}
-              to={to}
-              pickupLocation={pickupLocation}
-              dropOffLocation={dropOffLocation}
-              pickupLocationName={pickupLocationName}
-              distance={distance}
-              navigation={navigation}
-              hidePrice={hidePrice}
-            />
+              {rows.map((car) => (
+                <Car
+                  key={car._id}
+                  car={car}
+                  booking={booking}
+                  pickupLocation={pickupLocation}
+                  dropOffLocation={dropOffLocation}
+                  from={from as Date}
+                  to={to as Date}
+                  pickupLocationName={pickupLocationName}
+                  distance={distance}
+                  hideSupplier={hideSupplier}
+                  sizeAuto={sizeAuto}
+                  hidePrice={hidePrice}
+                />
+              ))}
+            </>
           )}
-          keyExtractor={(item) => item._id}
-          onEndReachedThreshold={0.8}
-          onEndReached={() => {
-            if (fetch && !onScrollEnd) {
-              setOnScrollEnd(true)
-            }
-          }}
-          onMomentumScrollEnd={() => {
-            if (onScrollEnd && fetch) {
-              setPage(page + 1)
-            }
-            setOnScrollEnd(false)
-          }}
-          ListHeaderComponent={header}
-          ListFooterComponent={
-            <View style={styles.container}>
-              {
-                footerComponent || (fetch
-                  ? <ActivityIndicator size="large" color="#f37022" style={styles.indicator} />
-                  : <></>)
-              }
-            </View>
-          }
-          ListEmptyComponent={
-            !loading ? (
-              <View style={styles.container}>
-                <Text>{i18n.t('EMPTY_CAR_LIST')}</Text>
-              </View>
-            )
-              : <></>
-          }
-          refreshing={loading}
-          refreshControl={
-            <RefreshControl refreshing={refreshing} onRefresh={() => {
-              setRefreshing(true)
-
-              if ((routeName && pickupLocation && dropOffLocation && from && to) && ((routeName === 'Checkout' && cars && cars.length > 0) || routeName === 'Cars')) {
-                // helper.navigate(route, navigation, true)
-
-                navigation.dispatch((state) => {
-                  const { routes } = state
-                  const _routes = bookcarsHelper.cloneArray(routes) as NavigationRoute<StackParams, keyof StackParams>[]
-                  let index = 0
-
-                  if (routeName === 'Cars') {
-                    index = routes.findIndex((r) => r.name === 'Cars')
-                    // routes.splice(index, 1)
-                    const now = Date.now()
-                    _routes[index] = {
-                      name: routeName,
-                      key: `${routeName}-${now}`,
-                      params: {
-                        pickupLocation: pickupLocation!,
-                        dropOffLocation: dropOffLocation!,
-                        from: from!.getTime(),
-                        to: to!.getTime(),
-                        d: now,
-                      },
-                    }
-                    // routes.push({
-                    //   name: 'Cars',
-                    //   key: `Cars-${now}`,
-                    //   params: {
-                    //     pickupLocation: pickupLocation!,
-                    //     dropOffLocation: dropOffLocation!,
-                    //     from: from!.getTime(),
-                    //     to: to!.getTime(),
-                    //     d: now,
-                    //   },
-                    // })
-                  } else {
-                    index = routes.findIndex((r) => r.name === 'Checkout')
-                    // routes.splice(index, 1)
-                    const now = Date.now()
-                    _routes[index] = {
-                      name: routeName,
-                      key: `${routeName}-${now}`,
-                      params: {
-                        car: cars![0]._id,
-                        pickupLocation: pickupLocation!,
-                        dropOffLocation: dropOffLocation!,
-                        from: from!.getTime(),
-                        to: to!.getTime(),
-                        d: now,
-                      },
-                    }
-                    // routes.push({
-                    //   name: 'Checkout',
-                    //   key: `Checkout-${now}`,
-                    //   params: {
-                    //     car: cars![0]._id,
-                    //     pickupLocation: pickupLocation!,
-                    //     dropOffLocation: dropOffLocation!,
-                    //     from: from!.getTime(),
-                    //     to: to!.getTime(),
-                    //     d: now,
-                    //   },
-                    // })
-                  }
-
-                  return CommonActions.reset({
-                    ...state,
-                    routes: _routes,
-                    index,
-                  })
-                })
-              } else {
-                setRefreshing(false)
-              }
-            }}
-            />
-          }
-        />
+        {loading && <Progress />}
+      </section>
+      {env.PAGINATION_MODE === Const.PAGINATION_MODE.CLASSIC && !env.isMobile && (
+        <Pager page={page} pageSize={env.CARS_PAGE_SIZE} rowCount={rowCount} totalRecords={totalRecords} onNext={() => setPage(page + 1)} onPrevious={() => setPage(page - 1)} />
       )}
-    </View>
+    </>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  contentContainer: {
-    alignSelf: 'stretch',
-    paddingTop: 10,
-    paddingBottom: 10,
-  },
-  flatList: {
-    alignSelf: 'stretch',
-  },
-  indicator: {
-    margin: 10,
-  },
-})
 
 export default CarList
