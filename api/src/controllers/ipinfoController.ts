5c8e7e7e89c61f284b88d74112cc82c2912fad11
aelassas <akram.elassas@gmail.com>
2025-02-12 20:42:16 +0100

    Add unit tests for country code retrieval from IP

--- api/src/controllers/ipinfoController.ts
+++ api/src/controllers/ipinfoController.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from 'express'
-import i18n from '../lang/i18n'
 import * as ipinfoHelper from '../common/ipinfoHelper'
-import * as logger from '../common/logger'
 
 /**
  * Returns ISO 2 country code from IP.
@@ -12,12 +10,7 @@ import * as logger from '../common/logger'
  * @returns {unknown}
  */
 export const getCountryCode = async (req: Request, res: Response) => {
-  try {
-    const clientIp = ipinfoHelper.getClientIp(req)
-    const countryCode = await ipinfoHelper.getCountryCode(clientIp)
-    return res.json(countryCode)
-  } catch (err) {
-    logger.error(`[paypal.createPayPalOrder] ${i18n.t('ERROR')}`, err)
-    return res.status(400).send(i18n.t('ERROR') + err)
-  }
+  const clientIp = ipinfoHelper.getClientIp(req)
+  const countryCode = await ipinfoHelper.getCountryCode(clientIp)
+  return res.json(countryCode)
 }
