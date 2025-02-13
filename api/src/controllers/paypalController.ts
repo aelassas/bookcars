e3289faf85ec751dd789cb5b8c1e2fc35d3aca5e
aelassas <akram.elassas@gmail.com>
2025-02-13 11:53:02 +0100

    Fix: update admin email check to use double negation for clarity

--- api/src/controllers/paypalController.ts
+++ api/src/controllers/paypalController.ts
@@ -112,7 +112,7 @@ export const checkPayPalOrder = async (req: Request, res: Response) => {
       await bookingController.notify(user, booking.id, supplier, message)
 
       // Notify admin
-      const admin = env.ADMIN_EMAIL && (await User.findOne({ email: env.ADMIN_EMAIL, type: bookcarsTypes.UserType.Admin }))
+      const admin = !!env.ADMIN_EMAIL && (await User.findOne({ email: env.ADMIN_EMAIL, type: bookcarsTypes.UserType.Admin }))
       if (admin) {
         i18n.locale = admin.language
         message = i18n.t('BOOKING_PAID_NOTIFICATION')
