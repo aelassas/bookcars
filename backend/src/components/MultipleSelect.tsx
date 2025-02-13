32915a3eb1b9a5757cc00f11a8d2b87f44626dfa
aelassas <akram.elassas@gmail.com>
2025-02-13 11:09:35 +0100

    Fix: migrate deprecated MUI APIs

--- backend/src/components/MultipleSelect.tsx
+++ backend/src/components/MultipleSelect.tsx
@@ -372,10 +372,14 @@ const MultipleSelect = ({
             </li>
           )
         }}
-        ListboxProps={ListboxProps || undefined}
         onFocus={onFocus || undefined}
-        ListboxComponent={ListBox}
         onOpen={onOpen || undefined}
+        slotProps={{
+          listbox: {
+            component: ListBox,
+            ...ListboxProps
+          }
+        }}
       />
     </div>
   )
