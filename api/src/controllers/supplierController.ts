6b0122c89086874190880a598659a7e9b145596c
aelassas <akram.elassas@gmail.com>
2025-03-18 18:17:15 +0000

    Fix: enhance supplier car retrieval by adding supplierCarLimit and handling fully booked cars

--- api/src/controllers/supplierController.ts
+++ api/src/controllers/supplierController.ts
@@ -345,9 +345,11 @@ export const getFrontendSuppliers = async (req: Request, res: Response) => {
     const $match: mongoose.FilterQuery<bookcarsTypes.Car> = {
       $and: [
         { locations: pickupLocation },
-        { available: true }, { type: { $in: carType } },
+        { type: { $in: carType } },
         { gearbox: { $in: gearbox } },
         { fuelPolicy: { $in: fuelPolicy } },
+        { available: true },
+        { fullyBooked: { $in: [false, null] } },
       ],
     }
 
@@ -427,6 +429,52 @@ export const getFrontendSuppliers = async (req: Request, res: Response) => {
         {
           $match: $supplierMatch,
         },
+
+        // begining of supplierCarLimit -----------------------------------
+        {
+          // Add the "supplierCarLimit" field from the supplier to limit the number of cars per supplier
+          $addFields: {
+            maxAllowedCars: { $ifNull: ['$supplier.supplierCarLimit', Number.MAX_SAFE_INTEGER] }, // Use a fallback if supplierCarLimit is undefined
+          },
+        },
+        {
+          // Add a custom stage to limit cars per supplier
+          $group: {
+            _id: '$supplier._id', // Group by supplier
+            supplierData: { $first: '$supplier' },
+            cars: { $push: '$$ROOT' }, // Push all cars of the supplier into an array
+            maxAllowedCars: { $first: '$maxAllowedCars' }, // Retain maxAllowedCars for each supplier
+          },
+        },
+        {
+          // Limit cars based on maxAllowedCars for each supplier
+          $project: {
+            supplier: '$supplierData',
+            cars: {
+              $cond: {
+                if: { $eq: ['$maxAllowedCars', 0] }, // If maxAllowedCars is 0
+                then: [], // Return an empty array (no cars)
+                else: { $slice: ['$cars', 0, { $min: [{ $size: '$cars' }, '$maxAllowedCars'] }] }, // Otherwise, limit normally
+              },
+            },
+          },
+        },
+        {
+          // Flatten the grouped result and apply sorting
+          $unwind: '$cars',
+        },
+        {
+          // Ensure unique cars by grouping by car ID
+          $group: {
+            _id: '$cars._id',
+            car: { $first: '$cars' },
+          },
+        },
+        {
+          $replaceRoot: { newRoot: '$car' }, // Replace the root document with the unique car object
+        },
+        // end of supplierCarLimit -----------------------------------
+
         {
           $group: {
             _id: '$supplier._id',
