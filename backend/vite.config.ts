fe6e59e1bed5c0eb8db05264cedcc7dc3e6f1926
aelassas <akram.elassas@gmail.com>
2025-04-21 10:41:33 +0100

    Fix: increase file watch polling interval to 500ms in vite.config.ts

--- backend/vite.config.ts
+++ backend/vite.config.ts
@@ -42,7 +42,7 @@ export default ({ mode }: { mode: string }) => {
       port: Number.parseInt(process.env.VITE_PORT || '3001', 10),
       watch: {
         usePolling: true,
-        interval: 100,
+        interval: 500,
       },
       hmr: {
         protocol: 'ws',
