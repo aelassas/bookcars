086a00d118f2a50e98291b22e3d96bd230d64a3b
aelassas <akram.elassas@gmail.com>
2025-04-21 08:48:56 +0100

    Fix: update default server port in vite.config.ts to 3002

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -40,7 +40,7 @@ export default ({ mode }: { mode: string }) => {
 
     server: {
       host: '0.0.0.0',
-      port: Number.parseInt(process.env.VITE_PORT || '8080', 10),
+      port: Number.parseInt(process.env.VITE_PORT || '3002', 10),
       watch: {
         usePolling: true,
         interval: 100,
