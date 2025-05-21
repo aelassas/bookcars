8a94787e37b42a09f58053c15353de6cd6d6db7d
aelassas <akram.elassas@gmail.com>
2025-05-21 12:21:22 +0100

    dev(pre-commit): add ESLint configuration and lint script

---
+++ eslint.config.js
@@ -0,0 +1,63 @@
+import eslint from '@eslint/js'
+import globals from 'globals'
+
+const config = [
+  // Global ignores
+  {
+    ignores: [
+      '__config/',
+      '__scripts/',
+      '__services/',
+      '.github/',
+      '.husky/',
+      '.vscode/',
+      'api/',
+      'backend/',
+      'frontend/',
+      'mobile/',
+      'packages/',
+    ]
+  },
+  // Main configuration
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node
+      }
+    },
+    plugins: {},
+    rules: {
+      // ESLint core rules
+      'linebreak-style': 'off',
+      'no-underscore-dangle': 'off',
+      'no-restricted-syntax': 'off',
+      'max-len': 'off',
+      'indent': 'off',
+      'no-await-in-loop': 'off',
+      'no-console': 'off',
+      'object-curly-newline': 'off',
+      'semi': ['error', 'never'],
+      'brace-style': ['error', '1tbs'],
+      'curly': 'error',
+      'quotes': ['error', 'single', { 'avoidEscape': true }],
+      'no-multiple-empty-lines': 'error',
+      'no-multi-spaces': 'error',
+      'padded-blocks': ['error', 'never'],
+      'no-irregular-whitespace': 'error',
+
+      // Import plugin rules
+      'import/no-unresolved': 'off',
+      'import/extensions': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'import/prefer-default-export': 'off',
+
+      // Include rules from preset configurations
+      ...eslint.configs.recommended.rules,
+    },
+  }
+]
+
+export default config
