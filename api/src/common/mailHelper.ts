e721734199b08466342f1225491fd0be4e134d8c
aelassas <akram.elassas@gmail.com>
2025-06-07 22:04:04 +0100

    fix(mail): refactor sendMail to use test transporter in CI environment

--- api/src/common/mailHelper.ts
+++ api/src/common/mailHelper.ts
@@ -2,6 +2,23 @@ import nodemailer from 'nodemailer'
 import SMTPTransport from 'nodemailer/lib/smtp-transport'
 import * as env from '../config/env.config'
 
+const createTestTransporter = async () => {
+  // Create ethereal test account
+  let testAccount = await nodemailer.createTestAccount()
+
+  // Create transporter using test SMTP service
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: testAccount.user,
+      pass: testAccount.pass,
+    },
+  })
+
+  return transporter
+}
+
 /**
  * Send an email.
  *
@@ -9,25 +26,26 @@ import * as env from '../config/env.config'
  * @param {nodemailer.SendMailOptions} mailOptions
  * @returns {Promise<unknown>}
  */
-export const sendMail = (mailOptions: nodemailer.SendMailOptions): Promise<nodemailer.SentMessageInfo> => {
-    const transporterOptions: SMTPTransport.Options = {
-        host: env.SMTP_HOST,
-        port: env.SMTP_PORT,
-        auth: {
-            user: env.SMTP_USER,
-            pass: env.SMTP_PASS,
-        },
-    }
+export const sendMail = async (mailOptions: nodemailer.SendMailOptions): Promise<nodemailer.SentMessageInfo> => {
+  const transporterOptions: SMTPTransport.Options = {
+    host: env.SMTP_HOST,
+    port: env.SMTP_PORT,
+    auth: {
+      user: env.SMTP_USER,
+      pass: env.SMTP_PASS,
+    },
+  }
 
-    const transporter: nodemailer.Transporter = nodemailer.createTransport(transporterOptions)
+  const transporter: nodemailer.Transporter =
+    env.CI ? (await createTestTransporter()) : nodemailer.createTransport(transporterOptions)
 
-    return new Promise((resolve, reject) => {
-        transporter.sendMail(mailOptions, (err: Error | null, info: nodemailer.SentMessageInfo) => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(info)
-            }
-        })
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (err: Error | null, info: nodemailer.SentMessageInfo) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(info)
+      }
     })
+  })
 }
