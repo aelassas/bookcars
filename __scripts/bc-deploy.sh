b37723dffa761ad93dd328281d4e97bb24fc6cbe
aelassas <akram.elassas@gmail.com>
2025-06-17 07:04:47 +0100

    refactor: rename api folder to backend for clarity

--- __scripts/bc-deploy.sh
+++ __scripts/bc-deploy.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 if [ "$1" == "all" ]; then
-  /bin/bash /opt/bookcars/__scripts/bc-deploy-api.sh
+  /bin/bash /opt/bookcars/__scripts/bc-deploy-backend.sh
   /bin/bash /opt/bookcars/__scripts/bc-deploy-admin.sh
   /bin/bash /opt/bookcars/__scripts/bc-deploy-frontend.sh
 elif [ "$1" == "ui" ]; then
   /bin/bash /opt/bookcars/__scripts/bc-deploy-admin.sh
   /bin/bash /opt/bookcars/__scripts/bc-deploy-frontend.sh
-elif [ "$1" == "api" ]; then
-  /bin/bash /opt/bookcars/__scripts/bc-deploy-api.sh
+elif [ "$1" == "backend" ]; then
+  /bin/bash /opt/bookcars/__scripts/bc-deploy-backend.sh
 elif [ "$1" == "admin" ]; then
   /bin/bash /opt/bookcars/__scripts/bc-deploy-admin.sh
 elif [ "$1" == "frontend" ]; then
   /bin/bash /opt/bookcars/__scripts/bc-deploy-frontend.sh
 else
-  echo "Usage: bc-deploy all|ui|api|admin|frontend"
+  echo "Usage: bc-deploy all|ui|backend|admin|frontend"
 fi
