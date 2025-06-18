eb85e5daadf0d1e82c8a1a343d6fac266f0feb6a
aelassas <akram.elassas@gmail.com>
2025-06-18 18:34:15 +0100

    fix: update comment for clarity on pickup location and child locations in search results

--- backend/src/controllers/carController.ts
+++ backend/src/controllers/carController.ts
@@ -854,7 +854,7 @@ export const getFrontendCars = async (req: Request, res: Response) => {
       includeComingSoonCars,
     } = body
 
-    // include pickupLocation and child locations are included in search results
+    // include pickupLocation and child locations in search results
     const locIds = await Location.find({
       $or: [
         { _id: pickupLocation },
