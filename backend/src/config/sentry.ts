0509443d0b2096dc5e191f3f5d2a32e2f1bf7000
aelassas <akram.elassas@gmail.com>
2025-06-29 11:34:09 +0100

    fix(sentry):  remove tracesSampleRate

--- backend/src/config/sentry.ts
+++ backend/src/config/sentry.ts
@@ -6,10 +6,9 @@ import * as Sentry from '@sentry/node'
  * 
 */
 const initSentry = (): void => {
-  if (env.ENABLE_SENTRY && env.SENTRY_DSN_BACKEND) {
+  if (env.ENABLE_SENTRY) {
     Sentry.init({
       dsn: env.SENTRY_DSN_BACKEND,
-      tracesSampleRate: 1.0,
       environment: process.env.NODE_ENV || 'development',
       // Adds request headers and IP for users, for more info visit:
       // https://docs.sentry.io/platforms/javascript/guides/node/configuration/options/#sendDefaultPii
