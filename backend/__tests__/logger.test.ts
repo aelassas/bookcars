30e688746293c3bf040d754ffed0e8f6ef0ebcfe
aelassas <akram.elassas@gmail.com>
2025-06-29 17:08:49 +0100

    feat(tests): enhance logging tests with Sentry integration and TERM_PROGRAM checks

--- backend/__tests__/logger.test.ts
+++ backend/__tests__/logger.test.ts
@@ -1,15 +1,17 @@
-import 'dotenv/config'
-import * as logger from '../src/common/logger'
+import { jest } from '@jest/globals'
+import { Scope } from '@sentry/node'
 
 describe('Test logging', () => {
-  it('should test logging', () => {
+  it('should test logging', async () => {
     let res = true
     try {
+      let logger = await import('../src/common/logger.js')
       const obj = { ffo: 'bar' }
 
       // should show logs
       logger.info('info test')
       logger.info('info test', obj)
+      logger.info('info test', 'boom')
       logger.success('success test')
       logger.success('success test', obj)
       logger.warn('warn test')
@@ -36,10 +38,100 @@ describe('Test logging', () => {
       logger.error('error test')
       logger.error('error test', obj)
       logger.error('error test', new Error('test error'))
+
+      // test logger with TERM_PROGRAM environment variable
+      const originalTermProgram = process.env.TERM_PROGRAM
+      process.env.TERM_PROGRAM = ''
+      logger.info('ℹ️info ⚠️warn')
+      process.env.TERM_PROGRAM = 'vscode'
+      jest.resetModules()
+      logger = await import('../src/common/logger.js')
+      logger.info('ℹ️info ⚠️warn')
+      process.env.TERM_PROGRAM = originalTermProgram
     } catch {
       res = false
     }
     // test success
     expect(res).toBeTruthy()
+  })
+})
+
+describe('Test logger with sentry', () => {
+  beforeEach(() => {
+    jest.resetModules()
+  })
+
+  it('Should test logs when ENABLE_SENTRY is true', async () => {
+    jest.unstable_mockModule('../src/config/env.config.js', () => ({
+      ENABLE_SENTRY: true,
+      SENTRY_DSN_BACKEND: 'https://example@dsn.io/123',
+      SENTRY_TRACES_SAMPLE_RATE: 0.5,
+    }))
+
+    const captureExceptionMock = jest.fn()
+    const withScopeMock = jest.fn((callback: (scope: Scope) => void) => {
+      const mockScope = {
+        setLevel: jest.fn(),
+        setTag: jest.fn(),
+        setUser: jest.fn(),
+        setExtra: jest.fn(),
+      } as unknown as Scope
+
+      callback(mockScope)
+    })
+
+    jest.unstable_mockModule('@sentry/node', () => ({
+      captureException: captureExceptionMock,
+      captureMessage: jest.fn(),
+      withScope: withScopeMock,
+    }))
+
+    await jest.isolateModulesAsync(async () => {
+      const logger = await import('../src/common/logger.js')
+      logger.error('test error', new Error('boom'))
+      logger.error('test error', { message: 'boom' })
+      logger.error('test error')
+    })
+
+    const Sentry = await import('@sentry/node')
+    expect(Sentry.captureException).toHaveBeenCalledWith(expect.any(Error))
+    expect(Sentry.withScope).toHaveBeenCalled()
+  })
+
+  it('Should test logs when ENABLE_SENTRY is false', async () => {
+    jest.unstable_mockModule('../src/config/env.config.js', () => ({
+      ENABLE_SENTRY: false,
+      SENTRY_DSN_BACKEND: '',
+      SENTRY_TRACES_SAMPLE_RATE: 0.5,
+    }))
+
+    const captureExceptionMock = jest.fn()
+    const withScopeMock = jest.fn((callback: (scope: Scope) => void) => {
+      const mockScope = {
+        setTag: jest.fn(),
+        setUser: jest.fn(),
+        setExtra: jest.fn(),
+        setLevel: jest.fn(),
+      } as unknown as Scope
+
+      callback(mockScope)
+    })
+
+    jest.unstable_mockModule('@sentry/node', () => ({
+      captureException: captureExceptionMock,
+      captureMessage: jest.fn(),
+      withScope: withScopeMock,
+    }))
+
+    await jest.isolateModulesAsync(async () => {
+      const logger = await import('../src/common/logger.js')
+      logger.error('test error', new Error('boom'))
+      logger.error('test error', { message: 'boom' })
+      logger.error('test error')
+    })
+
+    const Sentry = await import('@sentry/node')
+    expect(Sentry.captureException).not.toHaveBeenCalled()
+    expect(Sentry.withScope).not.toHaveBeenCalled()
   })
 })
