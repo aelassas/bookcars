9e7238433fa46e55aab03645ebe4e22e97addfc1
aelassas <akram.elassas@gmail.com>
2025-06-30 18:53:30 +0100

    feat(tests): improve helper tests with JWT encryption and access token validation

--- backend/__tests__/helper.test.ts
+++ backend/__tests__/helper.test.ts
@@ -1,6 +1,9 @@
 import 'dotenv/config'
+import { jest } from '@jest/globals'
+import { SignJWT } from 'jose'
 import * as bookcarsTypes from ':bookcars-types'
 import * as helper from '../src/common/helper'
+import * as env from '../src/config/env.config'
 
 describe('Test string to boolean', () => {
   it('should convert a string to boolean', () => {
@@ -49,10 +52,51 @@ describe('Test getStripeLocale', () => {
   })
 })
 
+const jwtSecret = new TextEncoder().encode(env.JWT_SECRET)
+const jwtAlg = 'HS256'
+
+const encryptJWT = async (payload: any) => {
+  const jwt = await new SignJWT(payload)
+    .setProtectedHeader({ alg: jwtAlg })
+    .setIssuedAt()
+  const token = jwt.sign(jwtSecret)
+  return token
+}
+
 describe('Test validateAccessToken', () => {
   it('should test validateAccessToken', async () => {
+    const email = 'unknow@unknown.com'
+
     // test failure
-    expect(await helper.validateAccessToken('unknown' as bookcarsTypes.SocialSignInType, 'token', 'unknow@unknown.com')).toBeFalsy()
+    expect(await helper.validateAccessToken('unknown' as bookcarsTypes.SocialSignInType, 'token', email)).toBeFalsy()
+
+    // test success (facebook)
+    let payload = {}
+    let token = await encryptJWT(payload)
+    expect(await helper.validateAccessToken(bookcarsTypes.SocialSignInType.Facebook, token, email)).toBeTruthy()
+
+    // test success (apple)
+    payload = { email }
+    token = await encryptJWT(payload)
+    expect(await helper.validateAccessToken(bookcarsTypes.SocialSignInType.Apple, token, email)).toBeTruthy()
+
+    await jest.isolateModulesAsync(async () => {
+      // Mock axios with unstable_mockModule
+      await jest.unstable_mockModule('axios', () => ({
+        default: {
+          get: jest.fn().mockResolvedValue({ data: { email } } as unknown as never),
+        },
+      }))
+
+      // Import modules after mock is set (inside isolateModulesAsync)
+      const helper = await import('../src/common/helper') // adjust path
+
+      payload = { data: { email } }
+      token = await encryptJWT(payload)
+      expect(await helper.validateAccessToken(bookcarsTypes.SocialSignInType.Google, token, email)).toBeTruthy()
+      jest.resetModules()
+      jest.clearAllMocks()
+    })
   })
 })
 
